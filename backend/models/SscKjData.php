6427804e41d266ffca77ecd3d546a8800f7b7738
gaozi <433902+gaozi@users.noreply.gitee.com>
2019-05-17 15:29:16 +0800

    模型修改

--- backend/models/SscKjData.php
+++ backend/models/SscKjData.php
@@ -8,6 +8,7 @@ use Yii;
  * This is the model class for table "{{%ssc_kj_data}}".
  *
  * @property int $id
+ * @property int $index_id 顺序id
  * @property string $kj_code 开奖号码
  * @property string $code_str 开奖号码str
  * @property int $codes_hz 号码和值
@@ -33,7 +34,7 @@ use Yii;
  * @property int $type_3b 是否三兄弟
  * @property int $type_4b 是否四兄弟
  * @property int $type_4ds 单双：0非四单四双1四单2四双
- * @property int $lottery_type 彩种类型：1:1.5分 2:3分 3:5分 4:10分
+ * @property int $lottery_type 彩种类型：1:1.5分 2:3分 3:5分 4:10分|希腊、5:重庆ssc 6:新疆ssc
  * @property int $created_at 创建时间
  * @property string $update_time 创建时间
  * @property int $updated_at 更新时间
@@ -54,10 +55,11 @@ class SscKjData extends \common\models\base\BaseModel
     public function rules()
     {
         return [
-            [['codes_hz', 'codes_4nums_hz', 'code1', 'code2', 'code3', 'code4', 'code5', 'code_1_2', 'code_1_3', 'code_1_4', 'code_2_3', 'code_2_4', 'code_3_4', 'qihao', 'type_2', 'type_22', 'type_3', 'type_4', 'type_2b', 'type_3b', 'type_4b', 'type_4ds', 'lottery_type', 'created_at', 'updated_at'], 'integer'],
+            [['index_id', 'codes_hz', 'codes_4nums_hz', 'code1', 'code2', 'code3', 'code4', 'code5', 'code_1_2', 'code_1_3', 'code_1_4', 'code_2_3', 'code_2_4', 'code_3_4', 'qihao', 'type_2', 'type_22', 'type_3', 'type_4', 'type_2b', 'type_3b', 'type_4b', 'type_4ds', 'lottery_type', 'created_at', 'updated_at'], 'integer'],
             [['date', 'update_time'], 'safe'],
             [['kj_code'], 'string', 'max' => 8],
             [['code_str'], 'string', 'max' => 24],
+            [['qihao'], 'unique'],
         ];
     }
 
@@ -68,6 +70,7 @@ class SscKjData extends \common\models\base\BaseModel
     {
         return [
             'id' => 'ID',
+            'index_id' => '顺序id',
             'kj_code' => '开奖号码',
             'code_str' => '开奖号码str',
             'codes_hz' => '号码和值',
@@ -93,7 +96,7 @@ class SscKjData extends \common\models\base\BaseModel
             'type_3b' => '是否三兄弟',
             'type_4b' => '是否四兄弟',
             'type_4ds' => '单双：0非四单四双1四单2四双',
-            'lottery_type' => '彩种类型：1:1.5分 2:3分 3:5分 4:10分',
+            'lottery_type' => '彩种类型：1:1.5分 2:3分 3:5分 4:10分|希腊、5:重庆ssc 6:新疆ssc',
             'created_at' => '创建时间',
             'update_time' => '创建时间',
             'updated_at' => '更新时间',
