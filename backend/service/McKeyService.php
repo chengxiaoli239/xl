f5723f6973700ba68a7e080df9bb8e4b755022b1
gaozi <433902+gaozi@users.noreply.gitee.com>
2019-05-27 10:09:15 +0800

    数据统计处理

---
+++ backend/service/McKeyService.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: wangyegao
+ * Date: 2018/02/06
+ * Time: 09:40
+ */
+
+namespace backend\service;
+use  yii;
+
+class McKeyService{
+
+    public static function buildStaticMKey(){
+
+    }
+}
