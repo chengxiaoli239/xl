54fe65d659a4fcf10ca4a2c5319fc92e075df216
gaozi <433902+gaozi@users.noreply.gitee.com>
2019-05-30 09:45:26 +0800

    用户账号表

--- backend/models/TzSystemsUsers.php
+++ backend/models/TzSystemsUsers.php
@@ -9,6 +9,7 @@ use Yii;
  *
  * @property int $id
  * @property int $uid 用户id
+ * @property string $username 账号名称
  * @property int $tz_system_id 系统类型id，lt_tz_systems.id
  * @property string $sys_name 系统名称
  * @property string $account 投注账号
@@ -41,7 +42,7 @@ class TzSystemsUsers extends \common\models\base\BaseModel
             [['balance'], 'number'],
             [['updated_at'], 'required'],
             [['update_time'], 'safe'],
-            [['sys_name', 'account', 'ssc_domain'], 'string', 'max' => 64],
+            [['username', 'sys_name', 'account', 'ssc_domain'], 'string', 'max' => 64],
             [['password'], 'string', 'max' => 20],
             [['cookie'], 'string', 'max' => 640],
         ];
@@ -53,28 +54,20 @@ class TzSystemsUsers extends \common\models\base\BaseModel
     public function attributeLabels()
     {
         return [
-            'id' => Yii::t('app', 'ID'),
-            'uid' => Yii::t('app', '用户id'),
-            'tz_system_id' => Yii::t('app', '系统类型id，lt_tz_systems.id'),
-            'sys_name' => Yii::t('app', '系统名称'),
-            'account' => Yii::t('app', '投注账号'),
-            'password' => Yii::t('app', '网盘密码'),
-            'balance' => Yii::t('app', '系统余额'),
-            'status' => Yii::t('app', '系统开启状态'),
-            'ssc_domain' => Yii::t('app', '网盘地址'),
-            'cookie' => Yii::t('app', '登陆cookie'),
-            'created_at' => Yii::t('app', '创建时间'),
-            'updated_at' => Yii::t('app', '更新时间'),
-            'update_time' => Yii::t('app', '更新时间'),
+            'id' => 'ID',
+            'uid' => '用户id',
+            'username' => '账号名称',
+            'tz_system_id' => '系统类型id，lt_tz_systems.id',
+            'sys_name' => '系统名称',
+            'account' => '投注账号',
+            'password' => '网盘密码',
+            'balance' => '系统余额',
+            'status' => '系统开启状态',
+            'ssc_domain' => '网盘地址',
+            'cookie' => '登陆cookie',
+            'created_at' => '创建时间',
+            'updated_at' => '更新时间',
+            'update_time' => '更新时间',
         ];
-    }
-
-    /**
-     * @inheritdoc
-     * @return TzSystemsUsersQuery the active query used by this AR class.
-     */
-    public static function find()
-    {
-        return new TzSystemsUsersQuery(get_called_class());
     }
 }
