8795986f3c1a2a02be440a0ae8c98ff4a818f013
gaozi <wangyegao@126.com>
2019-06-12 21:00:10 +0800

    模型

--- backend/models/StaticCodeTypeArisePerdate.php
+++ backend/models/StaticCodeTypeArisePerdate.php
@@ -9,14 +9,14 @@ use Yii;
  *
  * @property int $id
  * @property string $date 日期
- * @property int $type_2 双重
- * @property int $type_3 三重
- * @property int $type_22 双双重
- * @property int $type_2b 两兄弟
- * @property int $type_3b 三兄弟
- * @property int $type_4b 四兄弟
- * @property int $type_2_type_2b 双重&两兄弟
- * @property int $type_2_type_3b 双重&三兄弟
+ * @property int $type_2 双重(29.41次)
+ * @property int $type_3 三重(2.19次)
+ * @property int $type_22 双双重(1.66次)
+ * @property int $type_2b 两兄弟(42.1次)
+ * @property int $type_3b 三兄弟(9.11次)
+ * @property int $type_4b 四兄弟(1.42次)
+ * @property int $type_2_type_2b 双重&两兄弟(15.77次)
+ * @property int $type_2_type_3b 双重&三兄弟(2.13次)
  * @property int $lottery_type 彩种类型：1:1.5分 2:3分 3:5分 4:10分|希腊、5:重庆ssc 6:新疆ssc
  * @property int $created_at 创建时间
  * @property int $updated_at 更新时间
@@ -53,14 +53,14 @@ class StaticCodeTypeArisePerdate extends \common\models\base\BaseModel
         return [
             'id' => 'ID',
             'date' => '日期',
-            'type_2' => '双重',
-            'type_3' => '三重',
-            'type_22' => '双双重',
-            'type_2b' => '两兄弟',
-            'type_3b' => '三兄弟',
-            'type_4b' => '四兄弟',
-            'type_2_type_2b' => '双重&两兄弟',
-            'type_2_type_3b' => '双重&三兄弟',
+            'type_2' => '双重(29.41次)',
+            'type_3' => '三重(2.19次)',
+            'type_22' => '双双重(1.66次)',
+            'type_2b' => '两兄弟(42.1次)',
+            'type_3b' => '三兄弟(9.11次)',
+            'type_4b' => '四兄弟(1.42次)',
+            'type_2_type_2b' => '双重&两兄弟(15.77次)',
+            'type_2_type_3b' => '双重&三兄弟(2.13次)',
             'lottery_type' => '彩种类型：1:1.5分 2:3分 3:5分 4:10分|希腊、5:重庆ssc 6:新疆ssc',
             'created_at' => '创建时间',
             'updated_at' => '更新时间',
