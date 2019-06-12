5a9087de1629dae6b4fdbc1c61e84d9ea624b596
gaozi <wangyegao@126.com>
2019-06-12 18:11:02 +0800

    号码类型

---
+++ backend/modules/forum/views/static-code-type-arise-perdate/index.php
@@ -0,0 +1,57 @@
+<?php
+
+use yii\helpers\Html;
+use yii\grid\GridView;
+
+/* @var $this yii\web\View */
+/* @var $searchModel backend\models\searchs\StaticCodeTypeArisePerdate */
+/* @var $dataProvider yii\data\ActiveDataProvider */
+
+$this->title = 'Static Code Type Arise Perdates';
+$this->params['breadcrumbs'][] = $this->title;
+?>
+<section class="static-code-type-arise-perdate-index wrapper site-min-height">
+    <!-- page start-->
+    <section class="panel">
+        <header class="panel-heading">
+            <?= Html::encode($this->title) ?>
+        </header>
+        <div class="panel-body">
+            <div class="adv-table editable-table ">
+                <div class="clearfix">
+                    <div class="btn-group">
+                        <?= Html::a('Create Static Code Type Arise Perdate', ['create'], ['class' => 'btn btn-success', 'style' => 'margin-bottom:15px;']) ?>
+                    </div>
+                </div>
+
+                <?php // echo $this->render('_search', ['model' => $searchModel]); ?>
+
+                <?= GridView::widget([
+                    'dataProvider' => $dataProvider,
+                    'filterModel' => $searchModel,
+                    'columns' => [
+                        ['class' => 'yii\grid\SerialColumn'],
+
+                        'id',
+                        'date',
+                        'type_2',
+                        'type_3',
+                        'type_22',
+                        //'type_2b',
+                        //'type_3b',
+                        //'type_4b',
+                        //'type_2_type_2b',
+                        //'type_2_type_3b',
+                        //'lottery_type',
+                        //'created_at',
+                        //'updated_at',
+                        //'update_time',
+
+                        ['class' => 'yii\grid\ActionColumn'],
+                    ],
+                ]); ?>
+            </div>
+        </div>
+    </section>
+    <!-- page end-->
+</section>
