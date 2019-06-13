df0fd94779e415f45ed92a2ae2a0c13634eef5cf
gaozi <wangyegao@126.com>
2019-06-13 14:27:01 +0800

    模型

--- backend/models/SscStaticVal.php
+++ backend/models/SscStaticVal.php
@@ -11,9 +11,7 @@ use Yii;
  * @property string $val 值
  * @property string $name 名字
  * @property int $status 是否显示0不显示1显示
- * @property int $type 类型：1和值2号码类型[例如:双双重、三重]
- * @property int $static_status 统计开关
- * @property int $start_days 默认统计前多少天开始
+ * @property int $type 类型：1和值2号码类型[例如:双双重、三重]3三字现带双重4四字现带双重5四字现不带双重
  * @property int $created_at 创建时间
  * @property int $updated_at 更新时间
  * @property string $update_time 更新时间
@@ -34,7 +32,7 @@ class SscStaticVal extends \common\models\base\BaseModel
     public function rules()
     {
         return [
-            [['status', 'type', 'static_status', 'start_days', 'created_at', 'updated_at'], 'integer'],
+            [['status', 'type', 'created_at', 'updated_at'], 'integer'],
             [['update_time'], 'safe'],
             [['val'], 'string', 'max' => 120],
             [['name'], 'string', 'max' => 64],
@@ -51,12 +49,19 @@ class SscStaticVal extends \common\models\base\BaseModel
             'val' => '值',
             'name' => '名字',
             'status' => '是否显示0不显示1显示',
-            'type' => '类型：1和值2号码类型[例如:双双重、三重]',
-            'static_status' => '统计开关',
-            'start_days' => '默认统计前多少天开始',
+            'type' => '类型：1和值2号码类型[例如:双双重、三重]3三字现带双重4四字现带双重5四字现不带双重',
             'created_at' => '创建时间',
             'updated_at' => '更新时间',
             'update_time' => '更新时间',
         ];
+    }
+
+    /**
+     * @inheritdoc
+     * @return SscStaticValQuery the active query used by this AR class.
+     */
+    public static function find()
+    {
+        return new SscStaticValQuery(get_called_class());
     }
 }
