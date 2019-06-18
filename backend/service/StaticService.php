e88cdbef2b82cd5bd649bd91a49e3cd0c476758c
gaozi <wangyegao@126.com>
2019-06-18 19:48:14 +0800

    号码类型遗漏更新

--- backend/service/StaticService.php
+++ backend/service/StaticService.php
@@ -1445,11 +1445,11 @@ class StaticService extends BaseService {
            # 号码类型：双重、双双重、四重、三兄弟、四兄弟
            $rst['updateCodeTypeYL'] = SscDataService::updateCodeTypeYL($type = 2, $lottery_type);
            # 三字现带双重
-           //$rst['updateCodeTypeYLs3'] = SscDataService::updateCodeTypeYLs($type = 3, $lottery_type);
+           $rst['updateCodeTypeYLs3'] = SscDataService::updateCodeTypeYLs($type = 3, $lottery_type);
            # 四字现带双重
-           //$rst['updateCodeTypeYLs4'] = SscDataService::updateCodeTypeYLs($type = 4, $lottery_type);
+           $rst['updateCodeTypeYLs4'] = SscDataService::updateCodeTypeYLs($type = 4, $lottery_type);
            # 四字现不带双重
-           //$rst['updateCodeTypeYLs5'] = SscDataService::updateCodeTypeYLs($type = 5, $lottery_type);
+           $rst['updateCodeTypeYLs5'] = SscDataService::updateCodeTypeYLs($type = 5, $lottery_type);
            StaticService::afterOpStatic($lottery_type, 'opAllCodeTypeYl');
        }
 
