a9359152afe5155b57662f185f5e5584595cd19e
gaozi <wangyegao@126.com>
2019-08-22 10:58:49 +0800

    管理员页面

--- backend/modules/forum/views/user-sys-plans/index_admin.php
+++ backend/modules/forum/views/user-sys-plans/index_admin.php
@@ -146,7 +146,7 @@ $profits_desc = date('Y-m-d').'系统投注利润：三定 : '.$sys_profits_3d;
                         'update_time',
 
                         //['class' => 'yii\grid\ActionColumn'],
-                        ['class' => 'yii\grid\ActionColumn','headerOptions'=>['width'=>'5%'],'template'=>'{update}&nbsp;&nbsp;&nbsp;&nbsp;{delete}'],
+                        ['class' => 'yii\grid\ActionColumn','headerOptions'=>['width'=>'5%'],'template'=>'{update}'],
                     ],
                 ]); ?>
             </div>
