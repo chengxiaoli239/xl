63991e2a74cb06ce07f046e102d7189506e162b0
gaozi <wangyegao@126.com>
2019-08-31 16:34:23 +0800

    号码类型

--- backend/modules/forum/views/ssc-static-yl/indexs.php
+++ backend/modules/forum/views/ssc-static-yl/indexs.php
@@ -25,48 +25,9 @@ $this->params['breadcrumbs'][] = $this->title;
                     </div>
                 </div-->
 
-                <div class="btn-group">
-                    <?= Html::a('号码类型', ['index', 'SscStaticYl[code_type]'=>1, 'SscStaticYl[type]'=>2], ['class' => 'btn btn-success', 'style' => 'margin-bottom:15px;']) ?>
-                </div>
-                ||
-                <div class="btn-group">
-                    <?= Html::a('三现带双', ['index', 'SscStaticYl[code_type]'=>2, 'SscStaticYl[type]'=>3, 'SscStaticYl[type_2]'=>1, 'SscStaticYl[type_3]'=>0], ['class' => 'btn btn-success', 'style' => 'margin-bottom:15px;']) ?>
-                </div>
-                <div class="btn-group">
-                    <?= Html::a('三现带双热码', ['index', 'SscStaticYl[code_type]'=>3, 'SscStaticYl[type]'=>3, 'SscStaticYl[type_2]'=>1, 'SscStaticYl[type_3]'=>0, 'SscStaticYl[is_hots]'=>1], ['class' => 'btn btn-success', 'style' => 'margin-bottom:15px;']) ?>
-                </div>
-                <div class="btn-group">
-                    <?= Html::a('三现三重', ['index', 'SscStaticYl[code_type]'=>4, 'SscStaticYl[type]'=>3, 'SscStaticYl[type_3]'=>1], ['class' => 'btn btn-success', 'style' => 'margin-bottom:15px;']) ?>
-                </div>
-                ||
-                <div class="btn-group">
-                    <?= Html::a('四现带双', ['index', 'SscStaticYl[code_type]'=>5, 'SscStaticYl[type]'=>4, 'SscStaticYl[type_2]'=>1, 'SscStaticYl[type_3]'=>0], ['class' => 'btn btn-success', 'style' => 'margin-bottom:15px;']) ?>
-                </div>
-                <div class="btn-group">
-                    <?= Html::a('四现带双热码', ['index', 'SscStaticYl[code_type]'=>501, 'SscStaticYl[type]'=>4, 'SscStaticYl[type_2]'=>1, 'SscStaticYl[type_3]'=>0], ['class' => 'btn btn-success', 'style' => 'margin-bottom:15px;']) ?>
-                </div>
-                ||
-                <div class="btn-group">
-                    <?= Html::a('四现不带双', ['index', 'SscStaticYl[code_type]'=>6, 'SscStaticYl[type]'=>4, 'SscStaticYl[type_2]'=>0], ['class' => 'btn btn-success', 'style' => 'margin-bottom:15px;']) ?>
-                </div>
-                <div class="btn-group">
-                    <?= Html::a('四现不带双热码', ['index', 'SscStaticYl[code_type]'=>601, 'SscStaticYl[type]'=>4, 'SscStaticYl[type_2]'=>0, 'SscStaticYl[is_hots]'=>1], ['class' => 'btn btn-success', 'style' => 'margin-bottom:15px;']) ?>
-                </div>
-                ||
-
-                <div class="btn-group">
-                    <?= Html::a('四兄弟', ['index', 'SscStaticYl[code_type]'=>7, 'SscStaticYl[type]'=>4, 'SscStaticYl[type_4b]'=>1], ['class' => 'btn btn-success', 'style' => 'margin-bottom:15px;']) ?>
-                </div>
-                <div class="btn-group">
-                    <?= Html::a('四单四双', ['index', 'SscStaticYl[code_type]'=>8, 'SscStaticYl[type]'=>4, 'SscStaticYl[type_2]'=>0, 'SscStaticYl[type_4ds]'=>1], ['class' => 'btn btn-success', 'style' => 'margin-bottom:15px;']) ?>
-                </div>
-                <div class="btn-group">
-                    <?= Html::a('四单带双', ['index', 'SscStaticYl[code_type]'=>9, 'SscStaticYl[type]'=>4, 'SscStaticYl[type_4d]'=>1, 'SscStaticYl[type_2]'=>1, 'SscStaticYl[type_3]'=>0], ['class' => 'btn btn-success', 'style' => 'margin-bottom:15px;']) ?>
-                </div>
-                <div class="btn-group">
-                    <?= Html::a('四双带双', ['index', 'SscStaticYl[code_type]'=>10, 'SscStaticYl[type]'=>4, 'SscStaticYl[type_4s]'=>1, 'SscStaticYl[type_2]'=>1, 'SscStaticYl[type_3]'=>0], ['class' => 'btn btn-success', 'style' => 'margin-bottom:15px;']) ?>
-                </div>
-
+                <?php
+                include(dirname(__FILE__).'/code_type_tab.php');
+                ?>
 
                 <?php // echo $this->render('_search', ['model' => $searchModel]); ?>
 
