c706949de5a33866f5f52306790cf7ed715fb897
gaozi <wangyegao@126.com>
2019-09-18 18:01:13 +0800

    新疆时时彩

--- common/kj/xjssc/XjSsc.php
+++ common/kj/xjssc/XjSsc.php
@@ -12,31 +12,13 @@ class XjSsc extends BaseKj {
     /**
      * @desc 7天-新疆
      * @param string $returnType
-     * @return bool
+     * @return array|bool
      */
     public static function getLotteryNoSevenDay($returnType = 'json'){
 
         if(!$kjData = self::getCurrentKjData(self::$lottery_type)){
-            $domain = BaseKj::getApiHost(13);
-            $url = $domain.'/kaijiang/list.aspx?lot=jxssc';
-            //$content = file_get_contents($url);
-            $h = str_replace('https://', '', $domain);
+            $datas = self::batchGrabSevenDay();
 
-            $headers = [
-               'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3',
-               'Accept-Encoding: gunzip, deflate, br',
-               'Accept-Language: zh-CN,zh;q=0.9,en;q=0.8',
-               'Connection: keep-alive',
-               'Cookie: Hm_lvt_afe1c3da922eb68bb36abb2f9a4ad0ce=1568795085; Hm_lpvt_afe1c3da922eb68bb36abb2f9a4ad0ce=1568795097',
-               'Host: '.$h,
-               'Upgrade-Insecure-Requests: 1',
-               'User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.100 Safari/537.36',
-            ];
-            $content = CurlService::getCurl($url, $headers);
-            $preg = "/<td>(.*?)<\/td><td>(.*?)<\/td><td>(.*?)<\/td>/ism"; // 这里是表达式，大神看看
-            preg_match_all($preg,$content,$matches);
-
-            $datas = $matches;
             $kjData = ['expect'=>$datas[1][0], 'opentime'=>$datas[2][0], 'opencode'=>$datas[3][0]];
         }
 
@@ -105,5 +87,33 @@ class XjSsc extends BaseKj {
         }else{
             return ['expect'=>$expect, 'opencode'=>$opencode, 'opentime'=>$opentime];
         }
+    }
+
+    /**
+     * @desc 七天 - 新疆 批量
+     * @return mixed
+     */
+    public static function batchGrabSevenDay(){
+        $domain = BaseKj::getApiHost(13);
+        $url = $domain.'/kaijiang/list.aspx?lot=jxssc';
+        //$content = file_get_contents($url);
+        $h = str_replace('https://', '', $domain);
+
+        $headers = [
+           'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3',
+           'Accept-Encoding: gunzip, deflate, br',
+           'Accept-Language: zh-CN,zh;q=0.9,en;q=0.8',
+           'Connection: keep-alive',
+           'Cookie: Hm_lvt_afe1c3da922eb68bb36abb2f9a4ad0ce=1568795085; Hm_lpvt_afe1c3da922eb68bb36abb2f9a4ad0ce=1568795097',
+           'Host: '.$h,
+           'Upgrade-Insecure-Requests: 1',
+           'User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.100 Safari/537.36',
+        ];
+        $content = CurlService::getCurl($url, $headers);
+        $preg = "/<td>(.*?)<\/td><td>(.*?)<\/td><td>(.*?)<\/td>/ism"; // 这里是表达式，大神看看
+        preg_match_all($preg,$content,$matches);
+
+        $datas = $matches;
+        return $datas;
     }
 }
