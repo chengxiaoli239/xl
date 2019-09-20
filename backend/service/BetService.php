cd5b31cf2ee1a051076cb0f31f987ce678148f4b
gaozi <wangyegao@126.com>
2019-09-20 11:46:09 +0800

    新疆时时彩投注

--- backend/service/BetService.php
+++ backend/service/BetService.php
@@ -51,7 +51,7 @@ abstract class BetService extends BaseBetService {
             if($lottery_type == 5){ # 0898体系重庆
                 $BetService = new HN0898Service($uid, $tz_system_id);
             }elseif ($lottery_type == 6){
-                $BetService = new HN0898Service6($uid, $tz_system_id);
+                $BetService = new Nine($uid, $tz_system_id);
             }
         }elseif(in_array($tz_system_id, [3])){
             # 3、重庆7时彩网
