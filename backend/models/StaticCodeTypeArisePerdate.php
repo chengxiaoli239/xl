39a2cc92c74cf68c885a21350b28d975e81fe233
gaozi <wangyegao@126.com>
2019-10-10 12:06:16 +0800

    model

--- backend/models/StaticCodeTypeArisePerdate.php
+++ backend/models/StaticCodeTypeArisePerdate.php
@@ -9,14 +9,14 @@ use Yii;
  *
  * @property int $id
  * @property string $date 日期
- * @property int $type_2 双重(29.41次)
- * @property int $type_3 三重(2.19次)
- * @property int $type_22 双双重(1.66次)
- * @property int $type_2b 两兄弟(42.1次)
- * @property int $type_3b 三兄弟(9.11次)
- * @property int $type_4b 四兄弟(1.42次)
- * @property int $type_2_type_2b 双重&两兄弟(15.77次)
- * @property int $type_2_type_3b 双重&三兄弟(2.13次)
+ * @property int $type_2 双重
+ * @property int $type_3 三重
+ * @property int $type_22 双双重
+ * @property int $type_2b 两兄弟
+ * @property int $type_3b 三兄弟
+ * @property int $type_4b 四兄弟
+ * @property int $type_2_type_2b 双重&两兄弟
+ * @property int $type_2_type_3b 双重&三兄弟
  * @property int $lottery_type 彩种类型：1:1.5分 2:3分 3:5分 4:10分|希腊、5:重庆ssc 6:新疆ssc
  * @property int $created_at 创建时间
  * @property int $updated_at 更新时间
@@ -51,20 +51,20 @@ class StaticCodeTypeArisePerdate extends \common\models\base\BaseModel
     public function attributeLabels()
     {
         return [
-            'id' => 'ID',
-            'date' => '日期',
-            'type_2' => '双重(29.41次)',
-            'type_3' => '三重(2.19次)',
-            'type_22' => '双双重(1.66次)',
-            'type_2b' => '两兄弟(42.1次)',
-            'type_3b' => '三兄弟(9.11次)',
-            'type_4b' => '四兄弟(1.42次)',
-            'type_2_type_2b' => '双重&两兄弟(15.77次)',
-            'type_2_type_3b' => '双重&三兄弟(2.13次)',
-            'lottery_type' => '彩种类型：1:1.5分 2:3分 3:5分 4:10分|希腊、5:重庆ssc 6:新疆ssc',
-            'created_at' => '创建时间',
-            'updated_at' => '更新时间',
-            'update_time' => '更新时间',
+            'id' => Yii::t('app', 'ID'),
+            'date' => Yii::t('app', '日期'),
+            'type_2' => Yii::t('app', '双重'),
+            'type_3' => Yii::t('app', '三重'),
+            'type_22' => Yii::t('app', '双双重'),
+            'type_2b' => Yii::t('app', '两兄弟'),
+            'type_3b' => Yii::t('app', '三兄弟'),
+            'type_4b' => Yii::t('app', '四兄弟'),
+            'type_2_type_2b' => Yii::t('app', '双重&两兄弟'),
+            'type_2_type_3b' => Yii::t('app', '双重&三兄弟'),
+            'lottery_type' => Yii::t('app', '彩种类型：1:1.5分 2:3分 3:5分 4:10分|希腊、5:重庆ssc 6:新疆ssc'),
+            'created_at' => Yii::t('app', '创建时间'),
+            'updated_at' => Yii::t('app', '更新时间'),
+            'update_time' => Yii::t('app', '更新时间'),
         ];
     }
 
