de4130674c79830f2a3e8bfa6268f29617bf0eb1
gaozi <wangyegao@126.com>
2019-10-16 00:37:48 +0800

    号码类型添加统计数据

--- backend/modules/forum/views/static-code4n-arise-month/code_type_tab.php
+++ backend/modules/forum/views/static-code4n-arise-month/code_type_tab.php
@@ -4,15 +4,15 @@ use yii\helpers\Html;
 ?>
 
 <div class="btn-group">
-    <?= Html::a('不带双一', ['index', 'SscStaticYl[lottery_type]'=>$lottery_type, 'SscStaticYl[type]'=>1, 'SscStaticYl[type_2]'=>0], ['class' => 'btn '.($type_2 == 0 && $type == 1 ? 'btn-success' : 'btn-default'), 'style' => 'margin-bottom:15px;']) ?>
+    <?= Html::a('不带双一', ['index', 'StaticCode4nAriseMonth[lottery_type]'=>$lottery_type, 'StaticCode4nAriseMonth[type]'=>1, 'StaticCode4nAriseMonth[type_2]'=>0], ['class' => 'btn '.($type_2 == 0 && $type == 1 ? 'btn-success' : 'btn-default'), 'style' => 'margin-bottom:15px;']) ?>
 </div>
 <div class="btn-group">
-    <?= Html::a('不带双二', ['index', 'SscStaticYl[lottery_type]'=>$lottery_type, 'SscStaticYl[type]'=>2, 'SscStaticYl[type_2]'=>0], ['class' => 'btn '.($type_2 == 0 && $type == 2 ? 'btn-success' : 'btn-default'), 'style' => 'margin-bottom:15px;']) ?>
+    <?= Html::a('不带双二', ['index', 'StaticCode4nAriseMonth[lottery_type]'=>$lottery_type, 'StaticCode4nAriseMonth[type]'=>2, 'StaticCode4nAriseMonth[type_2]'=>0], ['class' => 'btn '.($type_2 == 0 && $type == 2 ? 'btn-success' : 'btn-default'), 'style' => 'margin-bottom:15px;']) ?>
 </div>
 ||
 <div class="btn-group">
-    <?= Html::a('带双一', ['index', 'SscStaticYl[lottery_type]'=>$lottery_type, 'SscStaticYl[type]'=>1, 'SscStaticYl[type_2]'=>1], ['class' => 'btn '.($type_2 == 1 && $type == 1 ? 'btn-success' : 'btn-default'), 'style' => 'margin-bottom:15px;']) ?>
+    <?= Html::a('带双一', ['index', 'StaticCode4nAriseMonth[lottery_type]'=>$lottery_type, 'StaticCode4nAriseMonth[type]'=>1, 'StaticCode4nAriseMonth[type_2]'=>1], ['class' => 'btn '.($type_2 == 1 && $type == 1 ? 'btn-success' : 'btn-default'), 'style' => 'margin-bottom:15px;']) ?>
 </div>
 <div class="btn-group">
-    <?= Html::a('带双二', ['index', 'SscStaticYl[lottery_type]'=>$lottery_type, 'SscStaticYl[type]'=>2, 'SscStaticYl[type_2]'=>1], ['class' => 'btn '.($type_2 == 1 && $type == 2 ? 'btn-success' : 'btn-default'), 'style' => 'margin-bottom:15px;']) ?>
+    <?= Html::a('带双二', ['index', 'StaticCode4nAriseMonth[lottery_type]'=>$lottery_type, 'StaticCode4nAriseMonth[type]'=>2, 'StaticCode4nAriseMonth[type_2]'=>1], ['class' => 'btn '.($type_2 == 1 && $type == 2 ? 'btn-success' : 'btn-default'), 'style' => 'margin-bottom:15px;']) ?>
 </div>
