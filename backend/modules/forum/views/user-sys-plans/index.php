be6000b208a2e5db3fa4c9b3859a0540ae1ce958
gaozi <wangyegao@126.com>
2019-10-25 01:52:03 +0800

    四字现月统计

--- backend/modules/forum/views/user-sys-plans/index.php
+++ backend/modules/forum/views/user-sys-plans/index.php
@@ -165,9 +165,7 @@ $lottery_type_name = \common\service\CommonService::getLotteryName($lottery_type
                                     $str = \backend\models\ImportPlanCodes::findOne(['plan_id'=>$model->id])->codes;
                                     $txt = BaseStringHelper::truncate($str,25);
                                     $str = Html::a($txt, '#', ['title' => $str,'alt'=>$str]);
-                                }elseif($model->tz_type == 25){
-                                    $str = \backend\service\NumService::getDescByKuaixuan(json_decode($model->hz_Arr, true));
-                                }elseif($model->tz_type == 28){
+                                }elseif(in_array($model->tz_type, [25])){
                                     $str = \backend\service\NumService::getDescByKuaixuan(json_decode($model->hz_Arr, true));
                                 }else{
                                     $str = $model->hz_Arr;
