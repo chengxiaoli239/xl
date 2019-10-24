9152c75df5a4648f4e0579772009bc9c90ef93df
gaozi <wangyegao@126.com>
2019-10-24 17:32:56 +0800

    号码类型

--- backend/models/Num4Type.php
+++ backend/models/Num4Type.php
@@ -22,6 +22,11 @@ use Yii;
  * @property int $type_4b 是否四兄弟
  * @property int $type_4ds 单双：0非四单四双1四单2四双
  * @property int $type_log 是否对数
+ * @property int $type_3n_2b 三现:双重+兄弟
+ * @property int $type_3d 三单
+ * @property int $type_3s 三双
+ * @property int $type_4d 四单
+ * @property int $type_4s 四双
  * @property int $codes_hz 号码和值
  * @property int $created_at 创建时间
  * @property int $updated_at 更新时间
@@ -43,7 +48,7 @@ class Num4Type extends \common\models\base\BaseModel
     public function rules()
     {
         return [
-            [['code_1', 'code_2', 'code_3', 'code_4', 'type_2', 'type_22', 'type_3', 'type_4', 'type_2b', 'type_3b', 'type_4b', 'type_4ds', 'type_log', 'codes_hz', 'created_at', 'updated_at'], 'integer'],
+            [['code_1', 'code_2', 'code_3', 'code_4', 'type_2', 'type_22', 'type_3', 'type_4', 'type_2b', 'type_3b', 'type_4b', 'type_4ds', 'type_log', 'type_3n_2b', 'type_3d', 'type_3s', 'type_4d', 'type_4s', 'codes_hz', 'created_at', 'updated_at'], 'integer'],
             [['update_time'], 'safe'],
             [['code'], 'string', 'max' => 8],
         ];
@@ -70,6 +75,11 @@ class Num4Type extends \common\models\base\BaseModel
             'type_4b' => '是否四兄弟',
             'type_4ds' => '单双：0非四单四双1四单2四双',
             'type_log' => '是否对数',
+            'type_3n_2b' => '三现:双重+兄弟',
+            'type_3d' => '三单',
+            'type_3s' => '三双',
+            'type_4d' => '四单',
+            'type_4s' => '四双',
             'codes_hz' => '号码和值',
             'created_at' => '创建时间',
             'updated_at' => '更新时间',
