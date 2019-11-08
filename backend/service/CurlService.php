964ec74275400463059fac265ec1f23a8d087b35
gaozi <wangyegao@126.com>
2019-11-08 21:16:44 +0800

    投注

--- backend/service/CurlService.php
+++ backend/service/CurlService.php
@@ -126,6 +126,10 @@ class CurlService extends BaseService{
         //p(['url'=>$url, 'rstData'=>$rstData, 'data'=>$data, 'post_data'=>$post_data, 'headers'=>$headers, 'errno'=>$errno]);
         if(strpos($data, "\"Status\":1") !== false && strpos($data, "\"CompletedStatus\":1") !== false){ # json解析异常处理
             $rstData['Status'] = 1;
+        }else{
+            $logArr = ['url'=>$url, 'post_data'=>$post_data, 'header'=>$headers, 'rst'=>$data, 'errno'=>$errno];
+            //p($logArr);
+            Tool_Common::log('/WORK/LOG/'.Yii::$app->params['LOG_PATH'].'/'.date('Ymd').'/httpPostError','INFO','httpPost请求', $logArr);
         }
 
         if(strpos($data, '余额不足')){
