2a9d66da61292ef9d2da33b7ef9e4b3348b02e23
gaozi <wangyegao@126.com>
2020-04-13 21:24:16 +0800

    遗漏记录

--- backend/service/SscDataService.php
+++ backend/service/SscDataService.php
@@ -2316,8 +2316,8 @@ class SscDataService extends BaseService {
             $SscSdHzYl->static_nums = $Data['static_nums'];
             $SscSdHzYl->status = $Data['status'];
             //$SscDsYl->current_miss = $YL_data[$num];  // 1、当前遗漏次数
-            $SscSdHzYl->current_miss = $miss['current_times'];  // 1、当前遗漏次数
-            $SscSdHzYl->last_time_miss = $miss['last_times']; // 2、上次遗漏
+            $SscSdHzYl->current_miss = (string)$miss['current_times'];  // 1、当前遗漏次数
+            $SscSdHzYl->last_time_miss = (string)$miss['last_times']; // 2、上次遗漏
             $SscSdHzYl->last_time_miss_range = $miss['last_time_miss_range']; // 3、上次遗漏范围
             $SscSdHzYl->max_miss = $miss['max_miss'];      // 4、近200期内最大遗漏
             $SscSdHzYl->max_range = $miss['max_range']; // 5、200期内最大遗漏范围
