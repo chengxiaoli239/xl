dd41c7784b95dc1b454cc8b2f64d17cc24bb987b
gaozi <wangyegao@126.com>
2020-04-23 18:27:44 +0800

    代理IP

--- backend/service/Lucky5/LuckyBaseService.php
+++ backend/service/Lucky5/LuckyBaseService.php
@@ -2051,6 +2051,9 @@ class LuckyBaseService extends BaseTZService { # 重庆7时彩登陆体系
      */
     public static function setPoxy($ch, $url=''){
         $poxy_addr = PoxyIPService::getPoxyIp();
+        if(strpos($url, 'ww662889') !== false){
+            //$poxy_addr = '218.85.247.70:20000';
+        }
 
         if(!empty($poxy_addr)){
             //设置代理
