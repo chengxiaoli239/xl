c808bc92eb0ce8eb123788a559554e158dc60d76
gaozi <wangyegao@126.com>
2020-05-06 21:22:18 +0800

    数据库配置

--- common/config/main-local.php
+++ common/config/main-local.php
@@ -3,7 +3,7 @@ return [
     'components' => [
         'db' => [
             'class' => 'yii\db\Connection',
-            'dsn' => 'mysql:host=lottery_db_xl_host;dbname=lottery_xl',
+            'dsn' => 'mysql:host=127.0.0.1;dbname=lottery_xl',
             'username' => 'lottery_xl',
             'tablePrefix' => 'lt_',
             'password' => 'dPmk3frLf8Teb6wm',
