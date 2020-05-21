ffd2ddf2d8b13e6829701a4807a8342883a32b40
gaozi <wangyegao@126.com>
2020-05-21 09:40:01 +0800

    四定单双倍投

--- backend/modules/test/controllers/IndexController.php
+++ backend/modules/test/controllers/IndexController.php
@@ -170,10 +170,10 @@ class IndexController extends Controller
     }
 
     public function actionDw(){
+        $data = Lucky5::getLotteryLucky($type='json', $test = 1);p($data);
         $rst = PoxyIPService::kuaiIPValidTime(['116.115.210.176:16092', '121.56.39.180:20749']);p($rst);
         $rst = PoxyIPService::isValid(['116.115.210.176:16092']);d($rst);
         $data = XjSsc::getLotteryNoNineNum();p($data);
-        $data = Lucky5::getLotteryLucky();p($data);
         $str = "/App/ClearSession?errMsg=%e6%82%a8%e7%9a%84%e8%b4%a6%e5%8f%b7%e5%b7%b2%e5%9c%a8%e5%88%ab%e5%a4%84%e7%99%bb%e5%bd%95%e3%80%82";
         p(urldecode($str));
         $rst = PoxyIPService::getPoxyIp();p($rst);
