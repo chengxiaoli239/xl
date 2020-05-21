31cc3c255a0f459fce6f5bcb08e350457195ada7
gaozi <wangyegao@126.com>
2020-05-21 10:03:49 +0800

    模拟

--- backend/modules/forum/views/user-sys-plans/_form_3_22.php
+++ backend/modules/forum/views/user-sys-plans/_form_3_22.php
@@ -25,6 +25,10 @@ use yii\widgets\ActiveForm;
                     <?= $form->field($model, 'playway')->radioList([
                         '3'=>'四字定',
                     ])->label('投注方式') ?>
+                    <?= $form->field($model, 'is_test')->radioList([
+                        '0'=>'真实',
+                        '1'=>'模拟',
+                    ])->label('真实/模拟') ?>
 
                     <!--?= $form->field($model, 'status')->textInput() ?-->
                     <?= $form->field($model, 'status')->radioList([
