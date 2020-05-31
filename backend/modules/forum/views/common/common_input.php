1261968df0e8faf2156bab861b820bc231398e78
gaozi <wangyegao@126.com>
2020-06-01 00:29:21 +0800

    快捷

--- backend/modules/forum/views/common/common_input.php
+++ backend/modules/forum/views/common/common_input.php
@@ -1,5 +1,11 @@
 <?= $form->field($model, 'plan_type')->radioList($plan_types)->label('计划类型，为"止盈止损"计划时须填以下两项') ?>
-<?= $form->field($model, 'take_profits')->textInput($plan_types)->label('止盈点(正数，例：3000)') ?>
-<?= $form->field($model, 'stop_loss')->textInput($plan_types)->label('止损点(正数，例：4000)') ?>
+<div class="row">
+    <div class="col-lg-3">
+        <?= $form->field($model, 'take_profits')->textInput($plan_types)->label('止盈点(正数，例：3000)') ?>
+    </div>
+    <div class="col-lg-3">
+        <?= $form->field($model, 'stop_loss')->textInput($plan_types)->label('止损点(正数，例：4000)') ?>
+    </div>
+</div>
 
 <?= $form->field($model, 'tz_sites')->checkboxList($tz_sites_Arr)->label('投注站点') ?>
--- backend/modules/forum/views/user-sys-plans/_form_3_28.php
+++ backend/modules/forum/views/user-sys-plans/_form_3_28.php
@@ -20,18 +20,31 @@ use yii\widgets\ActiveForm;
 
                     <!--?= $form->field($model, 'account')->textInput(['maxlength' => true]) ?-->
 
-                    <?= $form->field($model, 'playway')->radioList([
-                        //'1'=>'二字定',
-                        //'2'=>'三字定',
-                        '3'=>'四字定',
-                    ])->label('投注方式') ?>
                     <input type="hidden" value="<?=$tz_type?>" name="UserSysPlans[tz_type]">
+                    <div class="row">
+                        <div class="col-lg-4">
+                            <?= $form->field($model, 'playway')->radioList([
+                                //'1'=>'二字定',
+                                //'2'=>'三字定',
+                                '3'=>'四字定',
+                            ])->label('投注方式') ?>
+                        </div>
+                        <div class="col-lg-4">
+                            <?= $form->field($model, 'is_test')->radioList([
+                                '0'=>'真实',
+                                '1'=>'模拟',
+                            ])->label('真实/模拟') ?>
+                        </div>
+                        <div class="col-lg-4">
+                            <!--?= $form->field($model, 'status')->textInput() ?-->
+                            <?= $form->field($model, 'status')->radioList([
+                                '0'=>'关闭',
+                                '1'=>'开启',
+                            ])->label('投注状态') ?>
+                        </div>
+                    </div>
 
-                    <!--?= $form->field($model, 'status')->textInput() ?-->
-                    <?= $form->field($model, 'status')->radioList([
-                        '0'=>'关闭',
-                        '1'=>'开启',
-                    ])->label('投注状态') ?>
+
 
                     <?= $form->field($model, 'single')->textInput() ?>
 
@@ -44,10 +57,16 @@ use yii\widgets\ActiveForm;
                     <?= $form->field($model, 'get_hzs')->checkboxList($hzArr)->label('和值【取】') ?>
                     <?= $form->field($model, 'remove_hzs')->checkboxList($hzArr)->label('和值【除】') ?>
 
-                    <?= $form->field($model, 'get_arises')->textInput()->label('上奖【取】') ?>
-                    <?= $form->field($model, 'remove_arises')->textInput()->label('上奖【除】') ?>
+                    <div class="row">
+                        <div class="col-lg-3">
+                            <?= $form->field($model, 'get_arises')->textInput()->label('上奖【取】') ?>
+                        </div>
+                        <div class="col-lg-3">
+                            <?= $form->field($model, 'remove_arises')->textInput()->label('上奖【除】') ?>
+                        </div>
+                    </div>
 
-                    <?= include (dirname(__FILE__).'/../common/common_input.php');?>
+                <?= include (dirname(__FILE__).'/../common/common_input.php');?>
 
                     <div class="form-group">
                         <div class="col-lg-offset-2 col-lg-10">
