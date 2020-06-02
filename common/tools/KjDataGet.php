ee9b7e91c40c29e7bbdf089ade690cfdb4192d18
gaozi <wangyegao@126.com>
2020-06-03 00:39:57 +0800

    日志

--- common/tools/KjDataGet.php
+++ common/tools/KjDataGet.php
@@ -151,7 +151,7 @@ class KjDataGet
                             $cache_time = 30*60;
                         }
                     }
-                    $logArr = ['data'=>$data, 'lottery_type'=>$lottery_type, /*'qihao'=>$qihao, 'kjData'=>$kjData, 'insertRst'=>$msg,*/ 'lottery'=>CqsscKcw::$lotteryNameArr[$kjConfig->lottery_type]];
+                    $logArr = ['data'=>$data, 'lottery_type'=>$lottery_type, /*'qihao'=>$qihao, 'kjData'=>$kjData, 'insertRst'=>$msg,'lottery'=>CqsscKcw::$lotteryNameArr[$kjConfig->lottery_type]*/ ];
                     Tool_Common::log('/WORK/LOG/'.Yii::$app->params['LOG_PATH'].'/'.date('Ymd').'/insertSscKjData', 'INFO', '开奖号码记录', $logArr);
                 }
                 $mkey_qihao = 'KJ_LOG_QIHAO_'.$kjConfig->lottery_type.'_'.$qihao;
@@ -161,7 +161,7 @@ class KjDataGet
                     $logArr['mkey_qihao'] = $mkey_qihao;
                     $logArr['qihao'] = $qihao;
                     $logArr['mkey'] = $mkey;
-                    $logArr['lottery_type'] = $lottery_type;
+                    //$logArr['lottery_type'] = $lottery_type;
                     Tool_Common::log('/WORK/LOG/'.Yii::$app->params['LOG_PATH'].'/'.date('Ymd').'/insertSscKjData', 'INFO', '开奖号码记录', $logArr);
                     $m->set($mkey, 1, $cache_time);
                     $m->set($mkey_qihao, 1, $cache_time);
@@ -321,7 +321,7 @@ class KjDataGet
         if (!$QxcKjData->save()) {
             $msg = current($QxcKjData->getErrors());
             $logArr = ['msg'=>$msg, 'qihao'=>$qihao, 'kjData'=>$kjData];
-            Tool_Common::log('/WORK/LOG/'.Yii::$app->params['LOG_PATH'].'/'.date('Ymd').'/insertQxcKjData', 'INFO', '开奖号码记录', $logArr);
+            Tool_Common::log('/WORK/LOG/'.Yii::$app->params['LOG_PATH'].'/'.date('Ymd').'/insertQxcKjData', 'INFO', '开奖号码记录-错误', $logArr);
             return ['status' => 300, 'msg' => $msg];
         }
         return ['status'=>200, 'msg'=>'开奖数据写入成功', 'insertRst'=>$insertRst];
