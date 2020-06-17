ec08b2263f4788257ce73e6db527b7964e2ebaea
gaozi <wangyegao@126.com>
2020-06-17 22:06:24 +0800

    计划类型

--- backend/modules/forum/views/user-sys-plans/_form_2_29.php
+++ backend/modules/forum/views/user-sys-plans/_form_2_29.php
@@ -98,22 +98,22 @@ use yii\widgets\ActiveForm;
                 </div>
 
                 <div class="row">
-                    <div class="col-lg-4 col-xs-6">
-                        <!--两数合、三数合-->
-                    <?= $form->field($model, 'no_fix_hefen_pos')->checkboxList([1=>'两数和',2=>'三数合'])->label('2.不定位合分') ?>
+                    <div class="col-lg-4 col-xs-4">
+                        <!--三定含除、取-->
+                        <?= $form->field($model, 'arise_in_sel')->checkboxList([1=>'除',2=>'取'])->label('2.三字定含') ?>
                     </div>
-                    <div class="col-lg-4 col-xs-6">
-                    <!--位置合分：合分-->
-                    <?= $form->field($model, 'no_fix_hefen')->textInput()->label('2.不定位合分:值')?>
+                    <div class="col-lg-4 col-xs-4">
+                        <?= $form->field($model, 'arise_in')->textInput()->label('2.三字定含')?>
                     </div>
                 </div>
                 <div class="row">
                     <div class="col-lg-4 col-xs-4">
-                    <!--三定含除、取-->
-                    <?= $form->field($model, 'arise_in_sel')->checkboxList([1=>'除',2=>'取'])->label('3.三字定含') ?>
+                        <!--两数合、三数合-->
+                    <?= $form->field($model, 'no_fix_hefen_pos')->checkboxList([1=>'两数',2=>'三数'])->label('3.不定位合分') ?>
                     </div>
                     <div class="col-lg-4 col-xs-4">
-                    <?= $form->field($model, 'arise_in')->textInput()->label('3.三字定含')?>
+                    <!--位置合分：合分-->
+                    <?= $form->field($model, 'no_fix_hefen')->textInput()->label('3.不定位合分:值')?>
                     </div>
                 </div>
 
