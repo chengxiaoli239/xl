075f2be43659c9c03df6804f5ac5f01720353c43
gaozi <wangyegao@126.com>
2020-07-12 02:09:00 +0800

    遗漏投

--- backend/modules/forum/views/user-sys-plans/index_admin.php
+++ backend/modules/forum/views/user-sys-plans/index_admin.php
@@ -177,7 +177,7 @@ $this->params['breadcrumbs'][] = $this->title;
                                 }
                                 $txt = BaseStringHelper::truncate($str,20);
                                 $str = Html::a($txt, '#', ['title' => $str,'alt'=>$str]);
-                                if($model->singles && in_array($model->plan_type,[2, 3, 4, 5])){
+                                if($model->singles && in_array($model->plan_type,[2, 3, 4, 5, 9])){
                                     $str .= '翻倍梯度:'.$model->singles;
                                 }
                                 return $str;
