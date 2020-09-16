160492f3aed5c2f551ccb2e2dd24cda550c50a8d
gaozi <wangyegao@126.com>
2020-09-17 00:59:06 +0800

    体育

--- backend/modules/cron/controllers/IndexController.php
+++ backend/modules/cron/controllers/IndexController.php
@@ -323,33 +323,6 @@ class IndexController extends Controller
 
         foreach ($TzSystemsUsers as $TzSystemsUser){
             $rst[$TzSystemsUser->id] = BaseService::login($TzSystemsUser->id);
-            /*
-            if(empty($TzSystemsUser->account) OR empty($TzSystemsUser->password)){
-                continue;
-            }
-            $tz_system_id = $TzSystemsUser->tz_system_id;
-            if(in_array($tz_system_id, [1,2])){
-                # 1、0898投注、2、99彩票网
-                $rst = HN0898Service::login($TzSystemsUser->uid, $TzSystemsUser->tz_system_id);
-            }elseif(in_array($tz_system_id, [3, 7])){
-                # 3、重庆7时彩网
-                if($tz_system_id == 3){
-                    $rst = SevenService::login($TzSystemsUser->uid, $TzSystemsUser->tz_system_id);
-                }else{
-                    $rst = LuckyBaseService::login($TzSystemsUser->uid, $TzSystemsUser->tz_system_id);
-                }
-            }elseif(in_array($tz_system_id, [4])){
-                # 4、7天彩票网
-            }elseif(in_array($tz_system_id, [5])){
-                # 5、希腊网
-            }elseif(in_array($tz_system_id, [6])){
-                # 6、会员网
-                $rst = HuiYuanBaseService::login($TzSystemsUser->uid, $TzSystemsUser->tz_system_id);
-            }elseif(in_array($tz_system_id, [8])){
-                # 8、麒麟财务系统网
-                $rst = QiLinBaseService::login($TzSystemsUser->uid, $TzSystemsUser->tz_system_id);
-            }
-            */
         }
         return $rst;
     }
@@ -361,5 +334,9 @@ class IndexController extends Controller
 
     }
 
+    public function actionGrabSportsGames(){
+        self::_init();
+
+    }
 
 }
---
+++ backend/service/sports/TennisSportsService.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace backend\service\sports;
+
+use common\tools\Tool_Common;
+use yii\helpers\ArrayHelper;
+use  yii;
+
+class TennisSportsService extends SportsBaseService  {
+    
+}
