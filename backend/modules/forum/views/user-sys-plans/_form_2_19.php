6281b9fb497c83bbb51fd32c01a4ec681fbf8fc7
gaozi <wangyegao@126.com>
2020-11-29 12:03:18 +0800

    三定遗漏投

--- backend/modules/forum/views/user-sys-plans/_form_2_19.php
+++ backend/modules/forum/views/user-sys-plans/_form_2_19.php
@@ -62,6 +62,11 @@ use yii\widgets\ActiveForm;
                     <!--?= $form->field($model, 'nums')->textInput() ?-->
 
                     <?= $form->field($model, 'singles')->textInput()->label('倍数梯度,如:0.1-0.3-0.7-1.5-3.1-6.2-12.5-25.1') ?>
+                    <div class="row">
+                        <div class="col-lg-3 col-xs-6">
+                            <?= $form->field($model, 'bet_while_miss')->textInput()->label('遗漏x期投,如:10') ?>
+                        </div>
+                    </div>
 
                     <?php include(dirname(__FILE__).'/take_or_stop_profits.php'); ?>
 
