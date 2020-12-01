ec041d4e31a77a6505c6416b6dc2fd747d7b1aa3
gaozi <wangyegao@126.com>
2020-12-02 01:56:38 +0800

    九九新网

--- backend/service/BaseService.php
+++ backend/service/BaseService.php
@@ -93,7 +93,7 @@ class BaseService{
         $tz_system_id = $TzSystemsUser->tz_system_id;
         # 是否有激活的计划
         $hasActivePlan = CommonService::hasPlansActiveSys($tz_system_id);
-        if(!$hasActivePlan && !in_array($tz_system_id, [14, 11, 3, 13])){
+        if(!$hasActivePlan && !in_array($tz_system_id, [3, 11, 12, 13, 14])){
             return false;
         }
 
