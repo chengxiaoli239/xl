c7aabb492249cbb69c37465005737a489ef0e13e
gaozi <wangyegao@126.com>
2020-12-07 23:45:42 +0800

    开奖号码

--- backend/models/SscKjData.php
+++ backend/models/SscKjData.php
@@ -19,6 +19,8 @@ use Yii;
  * @property int $code3 百位
  * @property int $code4 十位
  * @property int $code5 个位
+ * @property int $code6 6位
+ * @property int $code7 7位
  * @property int $code_1_2 1、2位和值
  * @property int $code_1_3 1、3位和值
  * @property int $code_1_4 1、4位和值
@@ -61,7 +63,7 @@ class SscKjData extends \common\models\base\BaseModel
     public function rules()
     {
         return [
-            [['index_id', 'codes_hz', 'codes_4nums_hz', 'code1', 'code2', 'code3', 'code4', 'code5', 'code_1_2', 'code_1_3', 'code_1_4', 'code_2_3', 'code_2_4', 'code_3_4', 'type_2', 'type_22', 'type_22b', 'type_3', 'type_4', 'type_2b', 'type_3b', 'type_4b', 'type_4ds', 'type_3n_2b', 'lottery_type', 'created_at', 'updated_at'], 'integer'],
+            [['index_id', 'codes_hz', 'codes_4nums_hz', 'code1', 'code2', 'code3', 'code4', 'code5', 'code6', 'code7', 'code_1_2', 'code_1_3', 'code_1_4', 'code_2_3', 'code_2_4', 'code_3_4', 'type_2', 'type_22', 'type_22b', 'type_3', 'type_4', 'type_2b', 'type_3b', 'type_4b', 'type_4ds', 'type_3n_2b', 'lottery_type', 'created_at', 'updated_at'], 'integer'],
             [['date', 'update_time'], 'safe'],
             [['kj_code', 'code_4n'], 'string', 'max' => 8],
             [['code_str', 'code_4n_str', 'qihao', 'code_3n'], 'string', 'max' => 24],
@@ -76,43 +78,45 @@ class SscKjData extends \common\models\base\BaseModel
     public function attributeLabels()
     {
         return [
-            'id' => 'ID',
-            'index_id' => '顺序id',
-            'kj_code' => '开奖号码',
-            'code_str' => '开奖号码str',
-            'code_4n_str' => '开奖前四位str',
-            'codes_hz' => '号码和值',
-            'codes_4nums_hz' => '前4位和值',
-            'code1' => '万位',
-            'code2' => '千位',
-            'code3' => '百位',
-            'code4' => '十位',
-            'code5' => '个位',
-            'code_1_2' => '1、2位和值',
-            'code_1_3' => '1、3位和值',
-            'code_1_4' => '1、4位和值',
-            'code_2_3' => '2、3位和值',
-            'code_2_4' => '2、4位和值',
-            'code_3_4' => '3、4位和值',
-            'code_1_2_3_4' => '四定单双',
-            'qihao' => '期号',
-            'date' => '开奖日期',
-            'code_3n' => '上奖三字现',
-            'code_4n' => '四字现',
-            'type_2' => '双重',
-            'type_22' => '双双重',
-            'type_22b' => '是否双两兄弟',
-            'type_3' => '三重',
-            'type_4' => '四重',
-            'type_2b' => '两兄弟',
-            'type_3b' => '三兄弟',
-            'type_4b' => '四兄弟',
-            'type_4ds' => '四定单双:0保留1四单2四双3两单两双4一单三双5一双三单',
-            'type_3n_2b' => '三现:双重&兄弟',
-            'lottery_type' => '彩种类型：1:1.5分 2:3分 3:5分 4:10分|希腊、5:重庆ssc 6:新疆ssc',
-            'created_at' => '创建时间',
-            'update_time' => '创建时间',
-            'updated_at' => '更新时间',
+            'id' => Yii::t('app', 'ID'),
+            'index_id' => Yii::t('app', '顺序id'),
+            'kj_code' => Yii::t('app', '开奖号码'),
+            'code_str' => Yii::t('app', '开奖号码str'),
+            'code_4n_str' => Yii::t('app', '开奖前四位str'),
+            'codes_hz' => Yii::t('app', '号码和值'),
+            'codes_4nums_hz' => Yii::t('app', '前4位和值'),
+            'code1' => Yii::t('app', '万位'),
+            'code2' => Yii::t('app', '千位'),
+            'code3' => Yii::t('app', '百位'),
+            'code4' => Yii::t('app', '十位'),
+            'code5' => Yii::t('app', '个位'),
+            'code6' => Yii::t('app', '6位'),
+            'code7' => Yii::t('app', '7位'),
+            'code_1_2' => Yii::t('app', '1、2位和值'),
+            'code_1_3' => Yii::t('app', '1、3位和值'),
+            'code_1_4' => Yii::t('app', '1、4位和值'),
+            'code_2_3' => Yii::t('app', '2、3位和值'),
+            'code_2_4' => Yii::t('app', '2、4位和值'),
+            'code_3_4' => Yii::t('app', '3、4位和值'),
+            'code_1_2_3_4' => Yii::t('app', '四定单双'),
+            'qihao' => Yii::t('app', '期号'),
+            'date' => Yii::t('app', '开奖日期'),
+            'code_3n' => Yii::t('app', '上奖三字现'),
+            'code_4n' => Yii::t('app', '四字现'),
+            'type_2' => Yii::t('app', '双重'),
+            'type_22' => Yii::t('app', '双双重'),
+            'type_22b' => Yii::t('app', '是否双两兄弟'),
+            'type_3' => Yii::t('app', '三重'),
+            'type_4' => Yii::t('app', '四重'),
+            'type_2b' => Yii::t('app', '两兄弟'),
+            'type_3b' => Yii::t('app', '三兄弟'),
+            'type_4b' => Yii::t('app', '四兄弟'),
+            'type_4ds' => Yii::t('app', '四定单双:0保留1四单2四双3两单两双4一单三双5一双三单'),
+            'type_3n_2b' => Yii::t('app', '三现:双重&兄弟'),
+            'lottery_type' => Yii::t('app', '彩种类型：1:1.5分 2:3分 3:5分 4:10分|希腊、5:重庆ssc 6:新疆ssc'),
+            'created_at' => Yii::t('app', '创建时间'),
+            'update_time' => Yii::t('app', '创建时间'),
+            'updated_at' => Yii::t('app', '更新时间'),
         ];
     }
 
