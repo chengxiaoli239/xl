5f0b2bc706b6e9b232835d94f1832ab71d9ea93f
gaozi <wangyegao@126.com>
2020-12-30 00:50:39 +0800

    宝塔配置表

--- backend/models/BtSystemConfigs.php
+++ backend/models/BtSystemConfigs.php
@@ -16,6 +16,7 @@ use Yii;
  * @property string $domain 地址
  * @property string $cookie 登陆cookie
  * @property string $user_agent 浏览器代理
+ * @property string $version 版本号
  * @property int $created_at 创建时间
  * @property int $updated_at 更新时间
  * @property string $update_time 更新时间
@@ -38,9 +39,8 @@ class BtSystemConfigs extends \common\models\base\BaseModel
         return [
             [['uid', 'status', 'created_at', 'updated_at'], 'integer'],
             [['cookie'], 'string'],
-            [['updated_at'], 'required'],
             [['update_time'], 'safe'],
-            [['sys_name', 'account', 'domain'], 'string', 'max' => 64],
+            [['sys_name', 'account', 'domain', 'version'], 'string', 'max' => 64],
             [['password'], 'string', 'max' => 20],
             [['user_agent'], 'string', 'max' => 640],
         ];
@@ -61,6 +61,7 @@ class BtSystemConfigs extends \common\models\base\BaseModel
             'domain' => Yii::t('app', '地址'),
             'cookie' => Yii::t('app', '登陆cookie'),
             'user_agent' => Yii::t('app', '浏览器代理'),
+            'version' => Yii::t('app', '版本号'),
             'created_at' => Yii::t('app', '创建时间'),
             'updated_at' => Yii::t('app', '更新时间'),
             'update_time' => Yii::t('app', '更新时间'),
