17156b8c89fa8fc50c6009a3177021fe67e87aca
gaozi <wangyegao@126.com>
2021-01-11 01:49:38 +0800

    优化

--- backend/modules/cron/controllers/IndexController.php
+++ backend/modules/cron/controllers/IndexController.php
@@ -241,7 +241,7 @@ class IndexController extends Controller
             $sleep_time = 5;
         }
         for($i=0; $i<$for_times; $i++){
-            if(in_array($uid, [32])){
+            if(in_array($uid, [32, 27])){
                 $rst['bet'] = BetService::betByUidNew($uid);
             }else{
                 $rst['bet'] = BetService::betByUid($uid); // 用户新计划投注，可正买可反买
