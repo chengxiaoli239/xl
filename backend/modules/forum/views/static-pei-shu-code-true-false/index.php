ba24b0dc964b195dfe3c246ec6152842fe07ff2d
gaozi <wangyegao@126.com>
2021-01-28 18:50:48 +0800

    配数

--- backend/modules/forum/views/static-pei-shu-code-true-false/index.php
+++ backend/modules/forum/views/static-pei-shu-code-true-false/index.php
@@ -39,24 +39,70 @@ $this->params['breadcrumbs'][] = $this->title;
                         'date',
                         'qihao',
                         'kj_code',
-                        'code_147_369',
-                        'code_259_369',
-                        'code_019_368',
-                        'code_123_678',
-                        'code_147_258',
-                        'code_017_348',
+                        //'code_147_369',
+                        ['attribute' => 'code_147_369',//'headerOptions'=>['width'=>'5%'],
+                            'format'=>'raw',
+                            'value' => function($model) {
+                                $txt = $model->code_147_369==1 ? "<font color='green'>√</font>" : "<font color='red'>X</font>";
+                                return Html::a($txt, 'javascript:;', ['title' => '√对X错']);
+                            }
+                        ],
+                        ['attribute' => 'code_259_369',//'headerOptions'=>['width'=>'5%'],
+                            'format'=>'raw',
+                            'value' => function($model) {
+                                $txt = $model->code_259_369==1 ? "<font color='green'>√</font>" :"<font color='red'>X</font>";
+                                return Html::a($txt, 'javascript:;', ['title' => '√对X错']);
+                            }
+                        ],
+                        //'code_019_368',
+                        ['attribute' => 'code_019_368',//'headerOptions'=>['width'=>'5%'],
+                            'format'=>'raw',
+                            'value' => function($model) {
+                                $txt = $model->code_019_368==1 ? "<font color='green'>√</font>" :"<font color='red'>X</font>";
+                                return Html::a($txt, 'javascript:;', ['title' => '√对X错']);
+                            }
+                        ],
+                        //'code_123_678',
+                        ['attribute' => 'code_123_678',//'headerOptions'=>['width'=>'5%'],
+                            'format'=>'raw',
+                            'value' => function($model) {
+                                $txt = $model->code_123_678==1 ? "<font color='green'>√</font>" :"<font color='red'>X</font>";
+                                return Html::a($txt, 'javascript:;', ['title' => '√对X错']);
+                            }
+                        ],
+                        //'code_147_258',
+                        ['attribute' => 'code_147_258',//'headerOptions'=>['width'=>'5%'],
+                            'format'=>'raw',
+                            'value' => function($model) {
+                                $txt = $model->code_147_258==1 ? "<font color='green'>√</font>" :"<font color='red'>X</font>";
+                                return Html::a($txt, 'javascript:;', ['title' => '√对X错']);
+                            }
+                        ],
+                        //'code_017_348',
+                        ['attribute' => 'code_017_348',//'headerOptions'=>['width'=>'5%'],
+                            'format'=>'raw',
+                            'value' => function($model) {
+                                $txt = $model->code_017_348==1 ? "<font color='green'>√</font>" :"<font color='red'>X</font>";
+                                return Html::a($txt, 'javascript:;', ['title' => '√对X错']);
+                            }
+                        ],
                         'code_456_789',
                         'code_012_789',
                         'code_345_678',
                         'code_357_019',
-                        'code_3b',
+                        //'code_3b',
+                        ['attribute' => 'code_3b','label'=>'三兄',//'headerOptions'=>['width'=>'5%'],
+                            'value' => function($model) {
+                                return $model->code_3b;
+                            }
+                        ],
                         //'lottery_type',
                         //'created_at',
                         //'updated_at',
                         //'update_time',
                         ['attribute' => 'update_time','label'=>'时间',//'headerOptions'=>['width'=>'5%'],
                             'value' => function($model) {
-                                return substr($model->update_time, 10,6);
+                                return substr($model->update_time, 10,9);
                             }
                         ],
 
