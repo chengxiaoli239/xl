81fcc5f30ed88888025411595e3a3e6daf682bd7
gaozi <wangyegao@126.com>
2021-01-29 23:41:54 +0800

    配数对错

--- backend/modules/forum/views/static-pei-shu-code-true-false/index.php
+++ backend/modules/forum/views/static-pei-shu-code-true-false/index.php
@@ -98,7 +98,7 @@ $this->params['breadcrumbs'][] = $this->title;
                         ['attribute' => 'code_012_789',//'headerOptions'=>['width'=>'5%'],
                             'format'=>'raw',
                             'value' => function($model) {
-                                $txt = $model->code_456_789==1 ? "<font color='green'>√</font>" :"<font color='red'>X</font>";
+                                $txt = $model->code_012_789==1 ? "<font color='green'>√</font>" :"<font color='red'>X</font>";
                                 return Html::a($txt, 'javascript:;', ['title' => '√对X错']);
                             }
                         ],
