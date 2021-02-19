0eac03ff4299ed9cbbbc910305566f8b28a71897
wangyegao <wangyegao@126.com>
2021-02-19 18:14:07 +0800

    测试git提交

--- backend/modules/test/controllers/IndexController.php
+++ backend/modules/test/controllers/IndexController.php
@@ -202,7 +202,6 @@ class IndexController extends Controller
 
     public function actionDw(){
         p('测试git提交');
-
         $xml = '<error><ret>1203</ret><message></message></error>';
         $data = WxService::xmlToArray($xml);p($data);
         $m = \Yii::$app->cache;
