ff1b52a87a6f57c075554c3daa641708fdc3b4bd
wangyegao <wangyegao@126.com>
2021-02-23 14:58:44 +0800

    调试

---
+++ common/config/main-local.php
@@ -0,0 +1,23 @@
+<?php
+return [
+    'components' => [
+        'db' => [
+            'class' => 'yii\db\Connection',
+            'dsn' => 'mysql:host=127.0.0.1;dbname=lottery_xl',
+            'enableSchemaCache' => true,
+            'schemaCacheDuration' => 3600,
+            'username' => 'lottery_xl',
+            'tablePrefix' => 'lt_',
+            'password' => 'dPmk3frLf8Teb6wm',
+            'charset' => 'utf8',
+        ],
+        'mailer' => [
+            'class' => 'yii\swiftmailer\Mailer',
+            'viewPath' => '@common/mail',
+            // send all mails to a file by default. You have to set
+            // 'useFileTransport' to false and configure a transport
+            // for the mailer to send real emails.
+            'useFileTransport' => true,
+        ],
+    ],
+];
