dee72ecd09aa43cd031b1db37ca1663fbcbb4938
wangyegao <wangyegao@126.com>
2021-02-23 14:57:58 +0800

    调试

--- backend/web/index.php
+++ backend/web/index.php
@@ -1,7 +1,7 @@
 <?php
 if($_SERVER['SERVER_PORT'] == '8090')@ini_set('session.name', 'PHPSESSID_BACKEND');# 一般不需要不提交SVN，只是为了在一个浏览器登录两个端口站点session不冲突问题
 defined('YII_DEBUG') or define('YII_DEBUG', true);
-defined('YII_ENV') or define('YII_ENV', 'dev');
+defined('YII_ENV') or define('YII_ENV', 'prod');
 
 defined('DEFAULT_LOTTERY_TYPE') or define('DEFAULT_LOTTERY_TYPE',6); # 默认为重庆时时彩
 
