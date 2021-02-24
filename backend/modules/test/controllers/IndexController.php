bbed9a98027a8436589f55614e45975829bdffb1
gaozi <wangyegao@126.com>
2021-02-24 23:45:03 +0800

    debug

--- backend/modules/test/controllers/IndexController.php
+++ backend/modules/test/controllers/IndexController.php
@@ -201,7 +201,10 @@ class IndexController extends Controller
     }
 
     public function actionDw(){
-        $sendRst = WxService::webwxsendmsg($uid=18, $fromUser = ['UserName'=>'@c005bd32a26f52268f83033ef5c74cd7a9c03ccabc822d17d9a498c78b65b833'], $to = '@a0b085715b825f16b937d6f1c0f7fe34', $word = 'xxx');p($sendRst);
+        $m = \Yii::$app->cache;
+        $mkey = WxService::buildWebWxNewLoginKey($uid=18);
+        //p($m->get($mkey));
+        $sendRst = WxService::webwxsendmsg($uid=18, $fromUser = ['UserName'=>'@4678d431465a45f2ee5129dbaa353482c8f678049f9f973e6a5deadcc130ed1f'], $to = '@a6c2666f2464f8c8628ebbf099f0749a', $word = 'xxx');p($sendRst);
         $callback = WxService::get_uri($uuid);p($callback);
         $xml = '<error><ret>1203</ret><message></message></error>';
         $data = WxService::xmlToArray($xml);p($data);
