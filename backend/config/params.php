85a3e7a34bcbcbd95496c4e90dad5351f265de78
gaozi <wangyegao@126.com>
2021-05-13 01:09:00 +0800

    重复号码

--- backend/config/params.php
+++ backend/config/params.php
@@ -29,7 +29,7 @@ return [
     'IMPORT_CODES_KUAIYI_UIDS' =>[], # aa22
 
     # 导入有重复的用户
-    'IMPORT_CODES_REPEAT_UIDS' =>[12], # aa22
+    'IMPORT_CODES_REPEAT_UIDS' =>[12, 14], # aa22
 
     # 现：二、三、四现
     'IS_XIAN' => [36, 17, 37],
