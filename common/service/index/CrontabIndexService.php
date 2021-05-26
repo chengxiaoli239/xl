a1cf2d9066d2806a8b612773400e1fe10b39a438
gaozi <wangyegao@126.com>
2021-05-27 02:22:46 +0800

    关盘时间

--- common/service/index/CrontabIndexService.php
+++ common/service/index/CrontabIndexService.php
@@ -12,7 +12,15 @@ class  CrontabIndexService{
             ->where(['AND',['=', 'u.status', 1], ['=', 'u.is_auto_login', 1], ['<>', 'u.ssc_domain', ''], ['=', 's.status', 1]])->all();
 
         foreach ($TzSystemsUsers as $TzSystemsUser){
-            $rst[$TzSystemsUser->id] = BaseService::login($TzSystemsUser->id);
+            $tz_system_id = $TzSystemsUser->tz_system_id;
+            $now_time = date('H:i');
+            $clock_times = [16=>'02:00'];
+            if(isset($clock_times[$tz_system_id]) && $now_time>$clock_times[$tz_system_id]){
+                $r = ['status'=>300, 'msg'=>'关盘时间'];
+            }else{
+                $r = BaseService::login($TzSystemsUser->id);
+            }
+            $rst[$TzSystemsUser->id] = $r;
         }
 
         return $rst;
