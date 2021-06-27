b3d94d1d1daa04d536de3d1ee2c5b84ee84851c1
gaozi <wangyegao@126.com>
2021-06-27 12:42:31 +0800

    调试

--- backend/modules/forum/views/user-sys-plans/import_codes.php
+++ backend/modules/forum/views/user-sys-plans/import_codes.php
@@ -1,10 +1,13 @@
 <div class="row" style="border-width:2px;margin-top:3px;border-style:solid;border-color: green;">
     <?= $form->field($model, 'change_per')->checkboxList([1=>'是'])->label('每期轮换') ?>
+<?php for($i=1; $i<=10; $i++){?>
 <div class="row import_codes_txts <?if (!$model->change_per) echo 'hide';?>">
     <div class="col-lg-12 col-xs-12">
-        <?= $form->field($model,"import_codes_txts[1]")->textarea([ 'autofocus' => false,'style'=>'height:150px' ])->label('1.号码一：多组英文逗号或空格隔开 2345,3456 或 2345 3456');?>
+        <?= $form->field($model,"import_codes_txts[$i]")->textarea([ 'autofocus' => false,'style'=>'height:150px' ])->label('号码'.$i.'：多组英文逗号或空格隔开 2345,3456 或 2345 3456');?>
     </div>
 </div>
+<?}?>
+    <!--
 <div class="row import_codes_txts <?if (!$model->change_per) echo 'hide';?>">
     <div class="col-lg-12 col-xs-12">
         <?= $form->field($model,"import_codes_txts[2]")->textarea([ 'autofocus' => false,'style'=>'height:150px' ])->label('2.号码二：多组英文逗号或空格隔开 2345,3456 或 2345 3456');?>
@@ -25,6 +28,7 @@
         <?= $form->field($model,"import_codes_txts[5]")->textarea([ 'autofocus' => false,'style'=>'height:150px' ])->label('5.号码五：多组英文逗号或空格隔开 2345,3456 或 2345 3456');?>
     </div>
 </div>
+-->
 </div>
 <script src="/chat_statics/js/jquery-1.8.0.min.js"></script>
 <script>
