624723eddf9392c8d3aae243d7a8b27a07b241a1
gaozi <wangyegao@126.com>
2021-07-04 20:58:30 +0800

    客户端

---
+++ backend/service/clients/ClientsBaseService.php
@@ -0,0 +1,14 @@
+<?php
+namespace backend\service\clients;
+
+use backend\models\SystemConfig;
+use backend\models\TzSystemsUsers;
+use backend\models\WxFriends;
+use backend\models\WxMsgTypes;
+use common\general\helpers\Curl;
+use common\tools\Tool_Common;
+
+class ClientsBaseService {
+
+
+}
---
+++ backend/service/clients/TzSystemUsersService.php
@@ -0,0 +1,37 @@
+<?php
+namespace backend\service\clients;
+
+use backend\models\TzSystemsUsers;
+use backend\service\BetService;
+use common\service\CommonService;
+use common\tools\Tool_Common;
+
+class TzSystemUsersService extends ClientsBaseService{
+
+    public static function getTzSystemUserLists($post=[]){
+        if(empty($post)){
+            return ['status'=>300, 'msg'=>'非法请求'];
+        }
+        $token = $post['access_token'];
+        if(empty($post['access_token'])){
+            return ['status'=>301, 'msg'=>'缺少access_token参数'];
+        }
+        $m = \Yii::$app->cache;
+        $mkey = 'getTzSystemUserLists_key';
+        $datas = $m->get($mkey);
+        if(empty($datas)){
+            $ADMIN_ACCESS_TOKEN = BetService::getConfig('ADMIN_ACCESS_TOKEN'); # 管理员token
+            $where = ['AND',['<>', 'status', -2], '1=1'];
+            if($ADMIN_ACCESS_TOKEN != $token){
+                $where = array_merge($where, [['=', 'access_token', $token]]);
+            }
+
+            $datas = TzSystemsUsers::find()->where($where)->asArray()->all();
+            $m->set($mkey, $datas, 180);
+        }
+
+        return ['status'=>200, 'datas'=>$datas, 'msg'=>'操作成功'];
+    }
+
+
+}
