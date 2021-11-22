2c109d182cb895861b8738f5b153da0010159453
wangyegao <wangyegao@126.com>
2021-11-22 19:19:49 +0800

    nums

--- backend/service/NumService.php
+++ backend/service/NumService.php
@@ -1357,8 +1357,9 @@ class NumService extends BaseService {
                                 $filter_index_ids[] = $index_id - $tmp_filter_index_Arr + 1;
                             }
                         }
+                        //p(['index_id'=>$index_id, $filters, $filter_index_ids]);
                         if(!empty($filter_index_ids)){ # 过滤期的index_id
-                            $SscKjDatas = SscKjData::find()->where(['AND', ['IN', 'index_id', $filter_index_ids], ['=', 'lottery_type', $lottery_type]])->asArray()->all();
+                            $SscKjDatas = SscKjData::find()->where(['AND', ['IN', 'index_id', $filter_index_ids], ['=', 'lottery_type', $lottery_type]])->orderBy(['id'=>SORT_DESC])->asArray()->all();
                             //p(['SscKjDatas'=>$SscKjDatas, 'filters'=>$filters, 'filter_index_ids'=>$filter_index_ids]);
                             foreach ($SscKjDatas as $sscKjData){
                                 if(!empty($filters['filter_pos1'])) { # 特殊过滤
