2b925904ef3fac9ba166c9ecb468525bb006ce26
gaozi <wangyegao@126.com>
2021-11-30 19:58:54 +0800

    代理

--- backend/models/TzSystemsUsers.php
+++ backend/models/TzSystemsUsers.php
@@ -34,6 +34,7 @@ use Yii;
  * @property string $desc 盘口状态
  * @property int $is_auto_bet 自动下注
  * @property int $is_use_proxy 使用代理
+ * @property int $proxy_type 代理类型
  * @property int $expire_time 到期时间
  * @property int $created_at 创建时间
  * @property int $updated_at 更新时间
@@ -55,7 +56,7 @@ class TzSystemsUsers extends \common\models\base\BaseModel
     public function rules()
     {
         return [
-            [['uid', 'is_agent', 'tz_system_id', 'status', 'is_auto_login', 'tz_sort', 'is_auto_bet', 'is_use_proxy', 'expire_time', 'created_at', 'updated_at'], 'integer'],
+            [['uid', 'is_agent', 'tz_system_id', 'status', 'is_auto_login', 'tz_sort', 'is_auto_bet', 'is_use_proxy', 'proxy_type', 'expire_time', 'created_at', 'updated_at'], 'integer'],
             [['balance', 'odds_2x', 'odds_3x', 'odds_4x', 'odds_2d', 'odds_3d', 'odds_4d'], 'number'],
             [['cookie', 'cookie_wx_web'], 'string'],
             [['updated_at'], 'required'],
@@ -73,37 +74,38 @@ class TzSystemsUsers extends \common\models\base\BaseModel
     public function attributeLabels()
     {
         return [
-            'id' => 'ID',
-            'uid' => '用户id',
-            'is_agent' => '是否代理',
-            'username' => '账号名称',
-            'tz_system_id' => '系统类型id，lt_tz_systems.id',
-            'sys_name' => '系统名称',
-            'account' => '投注账号',
-            'password' => '网盘密码',
-            'balance' => '系统余额',
-            'status' => '系统开启状态',
-            'is_auto_login' => '是否自动登陆',
-            'ssc_domain' => '网盘地址',
-            'cookie' => '登陆cookie',
-            'user_agent' => '浏览器代理',
-            'cookie_wx_web' => '微信web登录cookie',
-            'access_token' => '授权Token凭证',
-            'tz_sort' => '投注排序:从小到大',
-            'odds_2x' => '代理二现赔率',
-            'odds_3x' => '代理三现赔率',
-            'odds_4x' => '代理四现赔率',
-            'odds_2d' => '二定赔率',
-            'odds_3d' => '三定赔率',
-            'odds_4d' => '四定赔率',
-            'warn_val' => '预警值',
-            'desc' => '盘口状态',
-            'is_auto_bet' => '自动下注',
-            'is_use_proxy' => '使用代理',
-            'expire_time' => '到期时间',
-            'created_at' => '创建时间',
-            'updated_at' => '更新时间',
-            'update_time' => '更新时间',
+            'id' => Yii::t('app', 'ID'),
+            'uid' => Yii::t('app', '用户id'),
+            'is_agent' => Yii::t('app', '是否代理'),
+            'username' => Yii::t('app', '账号名称'),
+            'tz_system_id' => Yii::t('app', '系统类型id，lt_tz_systems.id'),
+            'sys_name' => Yii::t('app', '系统名称'),
+            'account' => Yii::t('app', '投注账号'),
+            'password' => Yii::t('app', '网盘密码'),
+            'balance' => Yii::t('app', '系统余额'),
+            'status' => Yii::t('app', '系统开启状态'),
+            'is_auto_login' => Yii::t('app', '是否自动登陆'),
+            'ssc_domain' => Yii::t('app', '网盘地址'),
+            'cookie' => Yii::t('app', '登陆cookie'),
+            'user_agent' => Yii::t('app', '浏览器代理'),
+            'cookie_wx_web' => Yii::t('app', '微信web登录cookie'),
+            'access_token' => Yii::t('app', '授权Token凭证'),
+            'tz_sort' => Yii::t('app', '投注排序:从小到大'),
+            'odds_2x' => Yii::t('app', '代理二现赔率'),
+            'odds_3x' => Yii::t('app', '代理三现赔率'),
+            'odds_4x' => Yii::t('app', '代理四现赔率'),
+            'odds_2d' => Yii::t('app', '二定赔率'),
+            'odds_3d' => Yii::t('app', '三定赔率'),
+            'odds_4d' => Yii::t('app', '四定赔率'),
+            'warn_val' => Yii::t('app', '预警值'),
+            'desc' => Yii::t('app', '盘口状态'),
+            'is_auto_bet' => Yii::t('app', '自动下注'),
+            'is_use_proxy' => Yii::t('app', '使用代理'),
+            'proxy_type' => Yii::t('app', '代理类型'),
+            'expire_time' => Yii::t('app', '到期时间'),
+            'created_at' => Yii::t('app', '创建时间'),
+            'updated_at' => Yii::t('app', '更新时间'),
+            'update_time' => Yii::t('app', '更新时间'),
         ];
     }
 
