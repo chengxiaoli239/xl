04880b2d197e38a54a7aa0dc35ba76b7b41824ea
gaozi <wangyegao@126.com>
2021-11-30 21:57:01 +0800

    ‘代理

--- common/service/proxy/ProxyDaiLiYunService.php
+++ common/service/proxy/ProxyDaiLiYunService.php
@@ -38,7 +38,7 @@ class ProxyDaiLiYunService {
 
         Tool_Common::log('/proxy/'.__FUNCTION__, 'INFO', '代理IP获取-代理云', ['url'=>$url, 'rst'=>$rst]);
 
-        return ['status'=>200, 'data'=>$rst['data'], 'msg'=>'代理IP数据获取成功'];
+        return ['status'=>200, 'data'=>$rst, 'msg'=>'代理IP数据获取成功'];
     }
 
     /**
@@ -58,7 +58,7 @@ class ProxyDaiLiYunService {
             if($data['status'] != 200) {
                 return $data;
             }
-            $ip_data = explode(',', $data);
+            $ip_data = explode(',', $data['data']);
             $ip_addr = $ip_data['0'];
             $ip = $ip_data[1];
             $city = $ip_data[2];
