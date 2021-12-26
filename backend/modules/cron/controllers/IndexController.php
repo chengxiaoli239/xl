f784f55da86c626548672096fed2c777ffcbc240
gaozi <wangyegao@126.com>
2021-12-26 18:51:12 +0800

    批量模拟下注

--- backend/modules/cron/controllers/IndexController.php
+++ backend/modules/cron/controllers/IndexController.php
@@ -282,9 +282,11 @@ class IndexController extends Controller
         $rst = ['status'=>200, 'msg'=>'操作成功'];
         $post = \Yii::$app->request->post();
 
-        for ($i=0; $i<5; $i++){
-            $rst['batch_simulate_data'] = BetService::batchSimulateBet($post['lottery_types'], $post['uid']);
-            sleep(10);
+        for ($i=0; $i<6; $i++){
+            $batch_simulate_data = BetService::batchSimulateBet($post['lottery_types'], $post['uid']);
+            $rst['batch_simulate_data'] = $batch_simulate_data;
+            Tool_Common::log('/datas/'.__FUNCTION__, 'INFO', '批量模拟下注', ['uid'=>$post['uid'], 'batch_simulate_data'=>$batch_simulate_data]);
+            sleep(5);
         }
 
         return $rst;
