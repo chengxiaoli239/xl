297a3831021f67130cae0959d9a14b508feb71ff
gaozi <wangyegao@126.com>
2021-12-27 18:31:49 +0800

    添加

--- backend/modules/test/controllers/IndexController.php
+++ backend/modules/test/controllers/IndexController.php
@@ -257,6 +257,7 @@ class IndexController extends Controller
     }
 
     public function actionDw(){
+        $current_qihao = NumService::getPlanBetCurrentQihao($plan_id='538', $lottery_type=8);p($current_qihao);
         $current_qihao = HN0898Service::getCurrentQihao($lottery_type=8); # 针对哪一期过滤，默认为：当前期号
         p($current_qihao);
         $arr = ['filter_type'=>1, 'filter_nums'=>1, 'playway'=>1, 'start_qihao'=>'20211224163', 'filter_poses'=>[1,2], 'lottery_type'=>DEFAULT_LOTTERY_TYPE]; # 过滤条件
