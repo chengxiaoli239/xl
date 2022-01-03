6a02f7b51f0c275dba8a2986f340d8bd75ba4b38
gaozi <wangyegao@126.com>
2022-01-03 14:38:14 +0800

    模拟

--- backend/service/SscDataService.php
+++ backend/service/SscDataService.php
@@ -3123,7 +3123,7 @@ class SscDataService extends BaseService {
                     $single = self::getPlanNextSingle($UserSysPlan->id, $codes_hz['singles_key'], $next_single_key, $lottery_type);
                 }
             }else{ # 不中奖
-                $history_max_miss = $codes_hz['history_max_miss']?:1; # 获取当前计划从统计开始到现在的遗漏，如果is_init = 0
+                $history_max_miss = $codes_hz['history_max_miss']?(int)$codes_hz['history_max_miss']:1; # 获取当前计划从统计开始到现在的遗漏，如果is_init = 0
                 if(in_array($UserSysPlan->plan_type, [9])) { # 遗漏倍投
                     $current_miss = $codes_hz['current_miss'] + 1; # 获取当前计划从统计开始到现在的遗漏，如果is_init = 0
                     $history_max_miss = max($current_miss, $history_max_miss);
