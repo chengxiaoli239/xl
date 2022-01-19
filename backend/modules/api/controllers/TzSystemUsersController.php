6333617aa95665a9b81f5d78d1f113a5817271fa
wangyegao <wangyegao@126.com>
2022-01-19 15:19:31 +0800

    通知

--- backend/modules/api/controllers/TzSystemUsersController.php
+++ backend/modules/api/controllers/TzSystemUsersController.php
@@ -208,7 +208,7 @@ class TzSystemUsersController extends Controller
         }
 
         $rst = BetService::pushTasksBetRst($post['plan_id'], $post['qihao'], $post['betRst'], $post['access_token'], $post['lottery_type']);
-        Tool_Common::log('/client_xy/'.__FUNCTION__, 'INFO', '游戏结果通知', ['account'=>$this->TzSystemsUsers['account'], 'post'=>$post, 'rst'=>$rst]);
+        Tool_Common::log('/client_xy/'.__FUNCTION__, 'INFO', '游戏结果通知', ['account'=>$this->TzSystemsUsers['username'], 'post'=>$post, 'rst'=>$rst]);
 
         return $rst;
     }
