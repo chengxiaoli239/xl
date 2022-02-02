964fcfeb20fb08c66a62ff4cb2f3a5f553c75031
wangyegao <wangyegao@126.com>
2022-02-02 17:36:07 +0800

    界面调整

--- backend/modules/forum/views/user-sys-plans/import_codes.php
+++ backend/modules/forum/views/user-sys-plans/import_codes.php
@@ -7,36 +7,12 @@
     </div>
 </div>
 <?php $turn_key = \Yii::$app->params['IMPORT_CODES_TURN']; for($i=1; $i<$turn_key; $i++){?>
-
-    <div class="row import_codes_txts <?if (!$model->change_per) echo 'hide';?>">
+<div class="row import_codes_txts <?if (!$model->change_per) echo 'hide';?>">
     <div class="col-lg-12 col-xs-12">
         <?= $form->field($model,"import_codes_txts[$i]")->textarea([ 'autofocus' => false,'style'=>'height:150px' ])->label('号码'.$i.'：多组英文逗号或空格隔开 2345,3456 或 2345 3456');?>
     </div>
 </div>
 <?}?>
-    <!--
-<div class="row import_codes_txts <?if (!$model->change_per) echo 'hide';?>">
-    <div class="col-lg-12 col-xs-12">
-        <?= $form->field($model,"import_codes_txts[2]")->textarea([ 'autofocus' => false,'style'=>'height:150px' ])->label('2.号码二：多组英文逗号或空格隔开 2345,3456 或 2345 3456');?>
-    </div>
-</div>
-<div class="row import_codes_txts <?if (!$model->change_per) echo 'hide';?>">
-    <div class="col-lg-12 col-xs-12">
-        <?= $form->field($model,"import_codes_txts[3]")->textarea([ 'autofocus' => false,'style'=>'height:150px' ])->label('3.号码三：多组英文逗号或空格隔开 2345,3456 或 2345 3456');?>
-    </div>
-</div>
-<div class="row import_codes_txts <?if (!$model->change_per) echo 'hide';?>">
-    <div class="col-lg-12 col-xs-12">
-        <?= $form->field($model,"import_codes_txts[4]")->textarea([ 'autofocus' => false,'style'=>'height:150px' ])->label('4.号码四：多组英文逗号或空格隔开 2345,3456 或 2345 3456');?>
-    </div>
-</div>
-<div class="row import_codes_txts <?if (!$model->change_per) echo 'hide';?>">
-    <div class="col-lg-12 col-xs-12">
-        <?= $form->field($model,"import_codes_txts[5]")->textarea([ 'autofocus' => false,'style'=>'height:150px' ])->label('5.号码五：多组英文逗号或空格隔开 2345,3456 或 2345 3456');?>
-    </div>
-</div>
--->
-</div>
 <script src="/chat_statics/js/jquery-1.8.0.min.js"></script>
 <script>
     $('#usersysplans-change_per').click(function () {
