e9d0e2c5e1d6748eaaca6a8f32464d1bda989a49
wangyegao <wangyegao@126.com>
2022-04-22 23:59:17 +0800

    pl

--- backend/service/BetService.php
+++ backend/service/BetService.php
@@ -2130,10 +2130,10 @@ abstract class BetService extends BaseBetService {
                     $end_time4 = microtime(true);
                     Tool_Common::log('/datas/'.__FUNCTION__.'_step', 'INFO', '下注步骤5', ['plan_id'=>$plan_id, 'rst'=>$rst, 'cs_time'=>($end_time4-$end_time3).'s']);
                     if($insertRst['status'] == 200){
-                        BetService::opOneBettingRecordAndHandlePlanStatic($insertRst['data']['record_id'], $plan_id, $current_qihao, $rst); # 处理开奖和计划相关
+                        $planStaticRst = BetService::opOneBettingRecordAndHandlePlanStatic($insertRst['data']['record_id'], $plan_id, $current_qihao, $rst); # 处理开奖和计划相关
                     }
                     $end_time5 = microtime(true);
-                    Tool_Common::log('/datas/'.__FUNCTION__.'_step', 'INFO', '下注处理结束', ['plan_id'=>$plan_id, 'rst'=>$rst, 'cs_time'=>($end_time5-$end_time4).'s', 'all_cs_time'=>($end_time5-$start_time1).'s']);
+                    Tool_Common::log('/datas/'.__FUNCTION__.'_step', 'INFO', '下注处理结束', ['plan_id'=>$plan_id, 'rst'=>$rst, 'planStaticRst'=>$planStaticRst, 'cs_time'=>($end_time5-$end_time4).'s', 'all_cs_time'=>($end_time5-$start_time1).'s']);
                 }catch (\Exception $exception){
                     Tool_Common::log('/datas/'.__FUNCTION__."_e", 'ERR', '计划模拟失败', ['plan_id'=>$plan_id, 'lottery_type'=>$lottery_type, 'err_msg'=>$exception->getMessage()]);
                 }
