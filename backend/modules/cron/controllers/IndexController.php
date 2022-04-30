8039c8640265232e616fd2220589c843cfde4286
wangyegao <wangyegao@126.com>
2022-04-30 21:39:12 +0800

    数据写入处理优化

--- backend/modules/cron/controllers/IndexController.php
+++ backend/modules/cron/controllers/IndexController.php
@@ -281,8 +281,8 @@ class IndexController extends Controller
         $rst = ['status'=>200, 'msg'=>'操作成功'];
         $post = \Yii::$app->request->post();
 
-        while (true){
-        //for ($i=0; $i<120; $i++){
+        //while (true){
+        for ($i=0; $i<120; $i++){
             $batch_simulate_data = BetService::batchSimulateBet($post['lottery_types'], $post['uid']);
             $rst['batch_simulate_data'] = $batch_simulate_data;
             Tool_Common::log('/datas/'.__FUNCTION__, 'INFO', '批量模拟下注', ['uid'=>$post['uid'], 'batch_simulate_data'=>$batch_simulate_data]);
