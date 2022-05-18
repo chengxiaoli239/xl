2667539371ddf22cb8349c568b836174635c8e20
gaozi <wangyegao@126.com>
2022-05-19 00:00:17 +0800

    球

---
+++ backend/models/sports/SportsPlatesGames.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace backend\models\sports;
+
+use Yii;
+
+/**
+ * This is the model class for table "{{%sports_plates_games}}".
+ *
+ * @property int $id
+ * @property string $plate_id 盘口id
+ * @property string $plate_name 盘口名称
+ * @property string $bet_url 下注明细页面链接
+ * @property string $league_matches_id 比赛id
+ * @property string $league_matches_name 比赛名称
+ * @property string $name1 队员1
+ * @property string $name1_path 元素1定位
+ * @property string $name2 队员1
+ * @property string $name2_path 元素2定位
+ * @property string $event_id 项目id
+ * @property string $desc 描述备注
+ * @property int $created_at 创建时间
+ * @property int $updated_at 更新时间
+ * @property string $update_time 更新时间
+ */
+class SportsPlatesGames extends \yii\db\ActiveRecord
+{
+    /**
+     * @inheritdoc
+     */
+    public static function tableName()
+    {
+        return '{{%sports_plates_games}}';
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function rules()
+    {
+        return [
+            [['desc'], 'string'],
+            [['created_at', 'updated_at'], 'integer'],
+            [['update_time'], 'safe'],
+            [['plate_id', 'plate_name', 'league_matches_id', 'league_matches_name', 'name1', 'name1_path', 'name2', 'name2_path', 'event_id'], 'string', 'max' => 64],
+            [['bet_url'], 'string', 'max' => 255],
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => Yii::t('app', 'ID'),
+            'plate_id' => Yii::t('app', '盘口id'),
+            'plate_name' => Yii::t('app', '盘口名称'),
+            'bet_url' => Yii::t('app', '下注明细页面链接'),
+            'league_matches_id' => Yii::t('app', '比赛id'),
+            'league_matches_name' => Yii::t('app', '比赛名称'),
+            'name1' => Yii::t('app', '队员1'),
+            'name1_path' => Yii::t('app', '元素1定位'),
+            'name2' => Yii::t('app', '队员1'),
+            'name2_path' => Yii::t('app', '元素2定位'),
+            'event_id' => Yii::t('app', '项目id'),
+            'desc' => Yii::t('app', '描述备注'),
+            'created_at' => Yii::t('app', '创建时间'),
+            'updated_at' => Yii::t('app', '更新时间'),
+            'update_time' => Yii::t('app', '更新时间'),
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     * @return SportsPlatesGamesQuery the active query used by this AR class.
+     */
+    public static function find()
+    {
+        return new SportsPlatesGamesQuery(get_called_class());
+    }
+}
---
+++ backend/models/sports/SportsPlatesGamesQuery.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace backend\models\sports;
+
+/**
+ * This is the ActiveQuery class for [[SportsPlatesGames]].
+ *
+ * @see SportsPlatesGames
+ */
+class SportsPlatesGamesQuery extends \yii\db\ActiveQuery
+{
+    /*public function active()
+    {
+        return $this->andWhere('[[status]]=1');
+    }*/
+
+    /**
+     * @inheritdoc
+     * @return SportsPlatesGames[]|array
+     */
+    public function all($db = null)
+    {
+        return parent::all($db);
+    }
+
+    /**
+     * @inheritdoc
+     * @return SportsPlatesGames|array|null
+     */
+    public function one($db = null)
+    {
+        return parent::one($db);
+    }
+}
