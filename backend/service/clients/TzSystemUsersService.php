b8624da3198356c05c7e93720a9a0eafefd9baf3
wangyegao <wangyegao@126.com>
2022-10-12 21:59:47 +0800

    close

--- backend/service/clients/TzSystemUsersService.php
+++ backend/service/clients/TzSystemUsersService.php
@@ -305,7 +305,7 @@ class TzSystemUsersService extends ClientsBaseService{
                             "Accept"=>"application/json, text/javascript, */*; q=0.01",
                             "Accept-Encoding"=>"gzip, deflate, br",
                             "Accept-Language"=>"zh-CN,zh;q=0.9",
-                            "Connection"=>"keep-alive",
+                            "Connection"=>"close",
                             'Content-Length' => (string)strlen(http_build_query($post_data)),
                             "Content-Type"=>"application/x-www-form-urlencoded; charset=UTF-8",
                             'Cookie' => $TzSystemsUsers->cookie,
