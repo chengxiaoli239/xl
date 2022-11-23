3800375cbe40dbad7aaf614be524b211cbbbdfc8
wangyegao <wangyegao@126.com>
2022-11-23 22:31:37 +0800

    队列

--- backend/service/jobs/TestJob.php
+++ backend/service/jobs/TestJob.php
@@ -5,6 +5,7 @@ class TestJob extends \yii\base\BaseObject implements \yii\queue\JobInterface
 {
     public $url;
     public $file;
+    public $txt;
 
     public function execute($data) {
         \common\tools\Tool_Common::log('/jobs/'.__FUNCTION__, 'INFO', '测试队列TestJob', $data);
