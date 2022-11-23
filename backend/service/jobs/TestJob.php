146b84d2f97faf082b4b1c67d88ea767312ea5ec
wangyegao <wangyegao@126.com>
2022-11-23 23:39:45 +0800

    队列

--- backend/service/jobs/TestJob.php
+++ backend/service/jobs/TestJob.php
@@ -5,13 +5,16 @@ use common\service\jobs\CommonJob;
 
 class TestJob extends CommonJob {
 
-    public static function getName($params)
-    {
+    public static function getName($params) {
         self::$name = '测试队列TestJob';
         return self::$name;
     }
 
     public function exec($params) {
+        return self::handle($params);
+    }
+
+    public static function handle($params){
         \common\tools\Tool_Common::log('/jobs/'.__FUNCTION__, 'INFO', '测试队列TestJob 进入', $params);
         // TODO: Implement exec() method.
 
