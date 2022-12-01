90829b14409aa17b48468e2efb997bbf21e0e5bd
gaozi <wangyegao@126.com>
2022-12-01 20:07:49 +0800

    队列

---
+++ backend/modules/forum/views/queue-log/_form.php
@@ -0,0 +1,62 @@
+<?php
+
+use yii\helpers\Html;
+use yii\widgets\ActiveForm;
+
+/* @var $this yii\web\View */
+/* @var $model common\models\QueueLog */
+/* @var $form yii\widgets\ActiveForm */
+?>
+
+<div class="queue-log-form row">
+    <div class="col-lg-12">
+        <section class="panel">
+            <header class="panel-heading">
+                <?= Html::encode($this->title) ?>
+            </header>
+            <div class="panel-body">
+                <?php $form = ActiveForm::begin(); ?>
+                    <?= $form->field($model, 'id')->textInput() ?>
+
+                    <?= $form->field($model, 'system_queue_id')->textInput(['maxlength' => true]) ?>
+
+                    <?= $form->field($model, 'business_id')->textInput(['maxlength' => true]) ?>
+
+                    <?= $form->field($model, 'params')->textInput(['maxlength' => true]) ?>
+
+                    <?= $form->field($model, 'remark')->textInput(['maxlength' => true]) ?>
+
+                    <?= $form->field($model, 'count')->textInput() ?>
+
+                    <?= $form->field($model, 'name')->textInput(['maxlength' => true]) ?>
+
+                    <?= $form->field($model, 'job_class')->textInput(['maxlength' => true]) ?>
+
+                    <?= $form->field($model, 'job_class_md5')->textInput(['maxlength' => true]) ?>
+
+                    <?= $form->field($model, 'status')->textInput() ?>
+
+                    <?= $form->field($model, 'time')->textInput() ?>
+
+                    <?= $form->field($model, 'last_push_time')->textInput() ?>
+
+                    <?= $form->field($model, 'complete_time')->textInput() ?>
+
+                    <?= $form->field($model, 'delay')->textInput() ?>
+
+                    <?= $form->field($model, 'type')->textInput(['maxlength' => true]) ?>
+
+                    <?= $form->field($model, 'create_time')->textInput() ?>
+
+                    <?= $form->field($model, 'update_time')->textInput() ?>
+
+                    <div class="form-group">
+                        <div class="col-lg-offset-2 col-lg-10">
+                            <?= Html::submitButton(Yii::t('app', 'Save'), ['class' => 'btn btn-danger']) ?>
+                        </div>
+                    </div>
+                <?php ActiveForm::end(); ?>
+            </div>
+        </section>
+    </div>
+</div>
---
+++ backend/modules/forum/views/queue-log/index.php
@@ -0,0 +1,60 @@
+<?php
+
+use yii\helpers\Html;
+use yii\grid\GridView;
+
+/* @var $this yii\web\View */
+/* @var $searchModel backend\models\searchs\QueueLog */
+/* @var $dataProvider yii\data\ActiveDataProvider */
+
+$this->title = Yii::t('app', 'Queue Logs');
+$this->params['breadcrumbs'][] = $this->title;
+?>
+<section class="queue-log-index wrapper site-min-height">
+    <!-- page start-->
+    <section class="panel">
+        <header class="panel-heading">
+            <?= Html::encode($this->title) ?>
+        </header>
+        <div class="panel-body">
+            <div class="adv-table editable-table ">
+                <div class="clearfix">
+                    <div class="btn-group">
+                        <?= Html::a(Yii::t('app', 'Create Queue Log'), ['create'], ['class' => 'btn btn-success', 'style' => 'margin-bottom:15px;']) ?>
+                    </div>
+                </div>
+
+                <?php // echo $this->render('_search', ['model' => $searchModel]); ?>
+
+                <?= GridView::widget([
+                    'dataProvider' => $dataProvider,
+                    'filterModel' => $searchModel,
+                    'columns' => [
+                        ['class' => 'yii\grid\SerialColumn'],
+
+                        'id',
+                        'system_queue_id',
+                        'business_id',
+                        'params',
+                        'remark',
+                        //'count',
+                        //'name',
+                        //'job_class',
+                        //'job_class_md5',
+                        //'status',
+                        //'time:datetime',
+                        //'last_push_time:datetime',
+                        //'complete_time:datetime',
+                        //'delay',
+                        //'type',
+                        //'create_time',
+                        //'update_time',
+
+                        ['class' => 'yii\grid\ActionColumn'],
+                    ],
+                ]); ?>
+            </div>
+        </div>
+    </section>
+    <!-- page end-->
+</section>
