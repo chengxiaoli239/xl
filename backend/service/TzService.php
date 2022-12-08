738eb874fadea77ad904bb3f5e72d5e21b5e664b
wangyegao <wangyegao@126.com>
2022-12-08 22:54:03 +0800

    任务

--- backend/service/TzService.php
+++ backend/service/TzService.php
@@ -188,7 +188,7 @@ class TzService extends BaseService {
             $rst['isCanOpStaticStatus'] = $isCanOpStaticStatus;
             Tool_Common::log('opSystemBetPlans','INFO','处理系统投注计划', $rst);
             StaticService::afterOpStatic($lottery_type, 'opSystemBetPlans');
-            push_queue(AfterRunSysPlansJob::class, ['lottery_type'=>$lottery_type, 'qihao'=>$qihao]);
+            push_queue(\common\service\jobs\kj_data\AfterRunSysPlansJob::class, ['lottery_type'=>$lottery_type, 'qihao'=>$qihao]);
         }catch (\Exception $e){
             Tool_Common::log('/static/'.__FUNCTION__, 'INFO', '数据统计异常', ['lottery_type'=>$lottery_type, 'err_msg'=>$e->getMessage()]);
         }
