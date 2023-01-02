0b8e05c77769af3582981c53269d700fa8764401
wangyegao <wangyegao@126.com>
2023-01-02 20:32:36 +0800

    语言包

--- backend/modules/forum/views/user-sys-plans/act-button.php
+++ backend/modules/forum/views/user-sys-plans/act-button.php
@@ -4,8 +4,8 @@ use yii\helpers\Html;
 ?>
 <div class="form-group">
     <div class="col-lg-offset-2 col-lg-10">
-        <?= Html::submitButton(Yii::t('app', 'Save').'xx', ['class' => 'btn btn-danger']) ?>
-        <?= Html::button(Yii::t('app', 'query-yl').'xx', ['class' => 'btn btn-success id-query', 'data-type'=>1]) ?>
+        <?= Html::submitButton(Yii::t('app', 'Save'), ['class' => 'btn btn-danger']) ?>
+        <?= Html::button(Yii::t('app', 'query-yl'), ['class' => 'btn btn-success id-query', 'data-type'=>1]) ?>
         <?= Html::button(Yii::t('app', 'query-profits-months'), ['class' => 'btn btn-success id-query-profits', 'data-type'=>2, 'data-static-type'=>1]) ?>
         <?= Html::button(Yii::t('app', 'query-profits-years'), ['class' => 'btn btn-success id-query-profits', 'data-type'=>2, 'data-static-type'=>2]) ?>
     </div>
