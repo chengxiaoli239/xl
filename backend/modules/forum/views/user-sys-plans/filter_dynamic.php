ff05cebe9a85b1735414f659f07115c1061a8d12
wangyegao <wangyegao@126.com>
2023-01-15 14:12:11 +0800

    动态过滤

--- backend/modules/forum/views/user-sys-plans/filter_dynamic.php
+++ backend/modules/forum/views/user-sys-plans/filter_dynamic.php
@@ -4,6 +4,6 @@
         <?= $form->field($model, 'is_filter_dynamic')->checkboxList(['1'=>'是'])->label('动态过滤') ?>
     </div>
     <div class="col-lg-10 col-xs-4">
-        <?= $form->field($model, 'filter_dynamic_types')->checkboxList(['1'=>'至少1小1大，剔除前一期号码至少2个上奖', 2=>'四双四单'])->label('类型') ?>
+        <?= $form->field($model, 'filter_dynamic_types')->checkboxList(['1'=>'1小1大，剔除前期号码至少2个上奖', 2=>'1小1大，剔除前期号码至少3个上奖'])->label('类型') ?>
     </div>
 </div>
