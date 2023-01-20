9981253e2b5caa7ad951bcfb123390e4207f05bd
wangyegao <wangyegao@126.com>
2023-01-21 00:15:17 +0800

    动态过滤

--- backend/service/NumService.php
+++ backend/service/NumService.php
@@ -3016,7 +3016,7 @@ class NumService extends BaseService {
                 }elseif (in_array($lottery_type, [1,17])){
                     $current_qihao = SscKjData::find()->where(['lottery_type'=>$lottery_type])->orderBy(['id'=>SORT_ASC])->limit(1)->asArray()->one()['qihao'];
                     if(($lottery_type==1 && $current_qihao>=2023008) OR ($lottery_type==17 && $current_qihao>=2023018)){
-                        return false;
+                        throw_info('最新期号截止');
                     }
                 }
                 if($r = BettingRecords::find()->where(['plan_id'=>$plan_id])->orderBy(['id'=>SORT_DESC])->limit(1)->one()){
