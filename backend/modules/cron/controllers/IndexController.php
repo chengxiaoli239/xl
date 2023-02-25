18f4c9b8e42e343ec3cd784fe9994d21476f0a7e
wangyegao <wangyegao@126.com>
2023-02-25 23:23:30 +0800

    模拟

--- backend/modules/cron/controllers/IndexController.php
+++ backend/modules/cron/controllers/IndexController.php
@@ -261,7 +261,7 @@ class IndexController extends Controller
         $post = \Yii::$app->request->post();
 
         //while (true){
-        for ($i=0; $i<55; $i++){
+        for ($i=0; $i<120; $i++){
             $batch_simulate_data = BetService::batchSimulateBet($post['lottery_types'], $post['uid']);
             $rst['batch_simulate_data'] = $batch_simulate_data;
             Tool_Common::log('/datas/'.__FUNCTION__, 'INFO', '批量模拟下注', ['uid'=>$post['uid'], 'batch_simulate_data'=>$batch_simulate_data]);
