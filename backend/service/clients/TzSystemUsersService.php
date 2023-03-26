b7f892bc4f5488da67433b68e0d1888c1ecf2973
wangyegao <wangyegao@126.com>
2023-03-26 12:31:03 +0800

    入列优化

--- backend/service/clients/TzSystemUsersService.php
+++ backend/service/clients/TzSystemUsersService.php
@@ -282,7 +282,7 @@ class TzSystemUsersService extends ClientsBaseService{
         $expect = $kjData['expect'];
         $flag = Lucky5::setKjDataCache($lottery_type, $expect, $kjData);
 
-        $params = ['lottery_type'=>$lottery_type, 'title'=>BetService::getLotteryName($lottery_type), 'is_grab_history'=>1];
+        $params = ['lottery_type'=>$lottery_type, 'title'=>BetService::getLotteryName($lottery_type).'_网盘推送', 'is_grab_history'=>1];
         push_queue(GrabKjDatasJob::class, $params);
 
         return ['status'=>200, 'data'=>['flag'=>$flag], 'msg'=>'操作成功'];
