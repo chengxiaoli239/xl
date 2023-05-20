23d9fc1f65a3c3235ea3ab17e3b66953c234a6d0
wangyegao <wangyegao@126.com>
2023-05-20 10:39:44 +0800

    文案

--- backend/service/clients/AgentClientsService.php
+++ backend/service/clients/AgentClientsService.php
@@ -47,48 +47,54 @@ class AgentClientsService extends ClientsBaseService{
             $now_time = time();
             list($code, $logDatas, $err_msg) = AgentClientsService::validateSyncMemberBetLogs($member_bet_logs);
             foreach ($logDatas as $logData){
-                if(!in_array($logData['account'], $flow_wp_accounts)){
-                    Tool_Common::log('/client_xy/'.__FUNCTION__, 'INFO', '不在跟随账号范围之内', ['account'=>$logData['account'], 'logData'=>$logData]);
-                    continue;
+                try {
+                    if(!in_array($logData['account'], $flow_wp_accounts)){
+                        throw_info('不在跟随账号范围之内');
+                    }
+                    $AgentUserBetLogs = AgentUserBetLogs::findOne(['access_token'=>$access_token, 'wp_record_id'=>$logData['log_member_quick_select_id']]);
+                    if(!empty($AgentUserBetLogs)){
+                        throw_info('日志记录已存在');
+                    }else{
+                        $AgentUserBetLogs = new AgentUserBetLogs();
+                    }
+                    list($playway, $kuaiyi_desc) = AgentClientsService::getKuaiYiDescByOperationLogs($logData['operation_content']);
+                    $single = number_format($logData['bet_money']/$logData['bet_count'], 2); # 倍数
+                    $setDatas = [
+                        'access_token' => $access_token,
+                        'uid' => $TzSystemsUsers->uid,
+                        'wp_record_id' => $logData['log_member_quick_select_id'],
+                        'member_id' => $logData['member_id'],
+                        'account' => $TzSystemsUsers->account,
+                        'bet_logs' => $logData['operation_content'], # 原始日志
+                        'bet_logs_n' => $kuaiyi_desc, # 转换后的快译
+                        'bet_counts' => $logData['bet_count'], # 原始下注组数
+                        'bet_money' => $logData['bet_money'], # 原始下注金额
+                        'bet_single' => $single,
+
+                        # 反买
+                        'bet_codes_op' => '', # 反买号码
+                        'bet_op_counts' => '', # 反买组数
+                        'bet_op_single' => $single, # 反买倍数
+                        'bet_op_money' => ($single * $bet_op_counts), # 反买金额
+                        'bet_type' => 1, # 下注类型：1反买2正买  默认反买
+
+                        'member_bet_time' => date('Y-').$logData['operation_datetime'],
+
+                        'tz_system_id' => 9,
+                        'created_at' => $now_time,
+                        'updated_at' => $now_time,
+                        'planway' => $playway,
+                        'log_type' => $logData['log_type'],  # 目前看都是102
+                    ];
+                    $AgentUserBetLogs->setAttributes($setDatas);
+                    Tool_Common::log('/client_xy/'.__FUNCTION__, 'INFO', '日志同步', ['logData'=>$logData, 'attributes'=>$AgentUserBetLogs->getAttributes()]);
+                    $flag = $AgentUserBetLogs->save();
+                    if(empty($flag)){
+                        throw_info($AgentUserBetLogs->getErrors());
+                    }
+                }catch (\Exception $e){
+                    Tool_Common::log('/client_xy/'.__FUNCTION__, 'INFO', '日志记录-异常', ['account'=>$logData['account'], 'flow_wp_accounts'=>$flow_wp_accounts, 'logData'=>$logData, 'err_msg'=>$e->getMessage()]);
                 }
-                $AgentUserBetLogs = AgentUserBetLogs::findOne(['access_token'=>$access_token, 'wp_record_id'=>$logData['log_member_quick_select_id']]);
-                if(!empty($AgentUserBetLogs)){
-                    throw_info('日志记录已存在');
-                }else{
-                    $AgentUserBetLogs = new AgentUserBetLogs();
-                }
-                list($playway, $kuaiyi_desc) = AgentClientsService::getKuaiYiDescByOperationLogs($logData['operation_content']);
-                $single = number_format($logData['bet_money']/$logData['bet_count'], 2); # 倍数
-                $setDatas = [
-                    'access_token' => $access_token,
-                    'uid' => $TzSystemsUsers->uid,
-                    'wp_record_id' => $logData['log_member_quick_select_id'],
-                    'member_id' => $logData['member_id'],
-                    'account' => $TzSystemsUsers->account,
-                    'bet_logs' => $logData['operation_content'], # 原始日志
-                    'bet_logs_n' => $kuaiyi_desc, # 转换后的快译
-                    'bet_counts' => $logData['bet_count'], # 原始下注组数
-                    'bet_money' => $logData['bet_money'], # 原始下注金额
-                    'bet_single' => $single,
-
-                    # 反买
-                    'bet_codes_op' => '', # 反买号码
-                    'bet_op_counts' => '', # 反买组数
-                    'bet_op_single' => $single, # 反买倍数
-                    'bet_op_money' => ($single * $bet_op_counts), # 反买金额
-                    'bet_type' => 1, # 下注类型：1反买2正买  默认反买
-
-                    'member_bet_time' => date('Y-').$logData['operation_datetime'],
-
-                    'tz_system_id' => 9,
-                    'created_at' => $now_time,
-                    'updated_at' => $now_time,
-                    'planway' => $playway,
-                    'log_type' => $logData['log_type'],  # 目前看都是102
-                ];
-                $AgentUserBetLogs->setAttributes($setDatas);
-                Tool_Common::log('/client_xy/'.__FUNCTION__, 'INFO', '日志同步', ['logData'=>$logData, 'attributes'=>$AgentUserBetLogs->getAttributes()]);
-                #$flag = $AgentUserBetLogs->save();
             }
 
             #$params = ['lottery_type'=>$lottery_type, 'title'=>BetService::getLotteryName($lottery_type).'_网盘推送bet日志', 'business_id'=>$expect];
