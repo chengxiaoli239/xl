fea9757ff663a8578695de112990589e325a69ab
wangyegao <wangyegao@126.com>
2023-07-17 17:05:03 +0800

    日志

--- backend/service/clients/AgentClientsService.php
+++ backend/service/clients/AgentClientsService.php
@@ -53,6 +53,7 @@ class AgentClientsService extends ClientsBaseService{
                 throw_info('跟随开关已关闭');
             }
             if($TzSystemsUsers->current_profits>0.00 && $TzSystemsUsers->stop_loss>0.00){
+                Tool_Common::log('/client_xy/'.__FUNCTION__, 'INFO', '止盈止损', ['account'=>$TzSystemsUsers->account, 'current_profits'=>$TzSystemsUsers->current_profits, 'take_profits'=>$TzSystemsUsers->take_profits, 'stop_loss'=>$TzSystemsUsers->stop_loss]);
                 if($TzSystemsUsers->current_profits>$TzSystemsUsers->take_profits OR $TzSystemsUsers->current_profits<(0-$TzSystemsUsers->stop_loss)){
                     throw_info('触发止盈止损，止盈：'.$TzSystemsUsers->current_profits.'，止损：'.$TzSystemsUsers->stop_loss.'，当前：'.$TzSystemsUsers->current_profits);
                 }
