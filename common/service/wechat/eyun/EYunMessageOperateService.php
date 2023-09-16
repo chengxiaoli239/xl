33051adb8b4e73b37ee034128306cf86cef4a893
wangyegao <wangyegao@126.com>
2023-09-17 00:34:13 +0800

    e云消息处理

---
+++ common/service/wechat/eyun/EYunMessageOperateService.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace common\service\wechat\eyun;
+
+use common\models\eyun\EyunAuth;
+use common\models\eyun\RobotUser;
+use common\service\BaseService;
+use common\service\chat\Tool_Common;
+use common\service\jobs\robots\EYunUserJobs;
+
+class EYunMessageOperateService  extends BaseService
+{
+    # 私聊
+    const MESSAGE_P_TEXT_CODE = '60001'; # 私聊文本
+    const MESSAGE_P_TEXT_CANCEL = '60018'; # 撤回消息
+
+    # 群聊
+    const MESSAGE_G_TEXT_CANCEL = '80018'; # 撤回消息
+    const MESSAGE_G_TEXT_CODE = '80001'; # 群聊文本
+
+    public function __construct($user_id='', $config = [])
+    {
+        $this->user_id = $user_id;
+        if(empty($config)){
+            $c = \Yii::$app->params['E_YUN'];
+            $config = [
+                'base_url' => $c['BASE_URL'],
+                'account' => $c['ACCOUNT'],
+                'password' => $c['PASSWORD'],
+                'ttuid' => $c['TTUID'],
+            ];
+            $this->base_url = $config['base_url'];
+            $this->ttuid = $config['ttuid'];
+            $this->account = $config['ttuid'];
+            $this->password = $config['password'];
+        }
+        $eyunAuth = EyunAuth::findOne(1);
+        if(!empty($eyunAuth)){
+            $headers = [
+                'Authorization' => $eyunAuth->authorization,
+            ];
+            $this->headers = $headers;
+        }
+        $RobotUser = RobotUser::findOne(['user_id'=>$user_id]);
+        if(!empty($RobotUser)){
+            $this->wcId = $RobotUser->wcId;
+            $this->wId = $RobotUser->wId;
+        }
+        parent::__construct($config);
+    }
+
+}
---
+++ common/service/wechat/eyun/api/EventServiceTrait.php
@@ -0,0 +1,54 @@
+<?php
+namespace common\service\wechat\eyun\api;
+
+use common\models\eyun\EYunMessage;
+use common\service\wechat\eyun\EYunBaseService;
+use common\service\wechat\eyun\EYunMessageOperateService;
+use common\tools\Tool_Common;
+use yii\helpers\Json;
+
+trait EventServiceTrait
+{
+    public static function eventHandler($data)
+    {
+        Tool_Common::log('/eyun/'.__FUNCTION__, 'INFO', '接收e云消息:', ['data'=>$data]);
+        $messageType = $data['messageType'];
+        $params = $data['data'];
+        $toUser = $params['toUser'];
+        $msgId = $params['msgId'];
+        $newMsgId = $params['newMsgId'];
+        $where = ['toUser'=>$toUser, 'msgId'=>$msgId, 'newMsgId'=>$newMsgId];
+        $EYunMessage = EYunMessage::findOne($where);
+        if(!empty($EYunMessage)){
+            return ['code'=>'1000', 'message'=>'消息接收成功'];
+        }
+        $now_time = time();
+        $EYunMessage = new EYunMessage();
+        $setData = [
+            'user_id' => EYunBaseService::getUserIdByFromUser($data['fromUser']),
+            'toUser'=>$toUser,
+            'msgId'=>$msgId,
+            'newMsgId'=>$newMsgId,
+            'status' => EYunMessage::STATUS_WAIT,
+            'data' => Json::encode($data, 320),
+            'created_at' => $now_time,
+            'updated_at' => $now_time,
+        ];
+        $EYunMessage->setAttributes($setData, false);
+        if(!$EYunMessage->save()){
+            return ['code'=>3000, 'message'=>Json::encode($EYunMessage->getErrors(), 320)];
+        }
+        switch ($messageType){
+            case EYunMessageOperateService::MESSAGE_P_TEXT_CODE: # 私聊
+                break;
+            case EYunMessageOperateService::MESSAGE_P_TEXT_CANCEL: # 私聊
+                break;
+            case EYunMessageOperateService::MESSAGE_G_TEXT_CODE: # 群聊
+                break;
+            case EYunMessageOperateService::MESSAGE_G_TEXT_CANCEL: # 群聊
+                break;
+        }
+
+        return ['code'=>'1000', 'message'=>'消息接收成功'];
+    }
+}
