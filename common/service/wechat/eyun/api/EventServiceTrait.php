79e13686599295f4547734d2ef33662c72e8bfc7
wangyegao <wangyegao@126.com>
2023-09-19 00:23:41 +0800

    消息处理

--- common/service/wechat/eyun/api/EventServiceTrait.php
+++ common/service/wechat/eyun/api/EventServiceTrait.php
@@ -13,12 +13,11 @@ trait EventServiceTrait
 {
     public static function eventHandler($data)
     {
+        $messageType = $data['messageType'];
+        $wcId = $data['wcId'];
         Tool_Common::log('/eyun/'.__FUNCTION__, 'INFO', '接收e云消息', ['messageType'=>$messageType, 'data'=>$data]);
         list($code, $dd, $msg) = self::saveMessage($data);
         $user_id = $dd['user_id'];
-        $messageType = $dd['messageType'];
-
-        $wcId = $data['wcId'];
 
         $data['user_id'] = $user_id;
         $data['business_id'] = $wcId;
