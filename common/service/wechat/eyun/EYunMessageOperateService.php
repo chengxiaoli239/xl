6bc2cdd406d34d0d6cacf2ee729242e17579e173
wangyegao <wangyegao@126.com>
2023-10-15 15:38:01 +0800

    优化消息

--- common/service/wechat/eyun/EYunMessageOperateService.php
+++ common/service/wechat/eyun/EYunMessageOperateService.php
@@ -196,7 +196,6 @@ class EYunMessageOperateService  extends EYunBaseService
      */
     public function receive($user_id='', $text='', $fromUser=''){
         try {
-            $replyTxt = '';
             #p([$user_id, $text]);
             $transaction = static::getDb()->beginTransaction();
             # 校验
@@ -222,7 +221,7 @@ class EYunMessageOperateService  extends EYunBaseService
             $now_time = time();
             $allMoneys = 0.00;
             $replyTxt = '【课号】：'.$qihao;
-            $replyTxt .= "\n【内容】：";
+            $replyTxt .= "\n【内容】：" . str_replace('元', '咪', $text);;
             foreach ($betCodeContents as $content){
                 if(empty($content['playMethod']['id'])){
                     throw_info('方式匹配为空，请按正确格式输入', ThirdDTypeService::CODE_FOR_USER);
@@ -254,8 +253,6 @@ class EYunMessageOperateService  extends EYunBaseService
             $replyTxt .= "\n【单号】：".$betOrderId.
 
             $replyTxt .= "\n【成功】 √  共：".$allMoneys.'咪';
-
-            $replyTxt = str_replace('元', '咪', $replyTxt);
 
             $transaction->commit();
             Tool_Common::log('/eyun/'.__FUNCTION__, 'INFO', '消息处理-成功', ['user_id'=>$user_id, 'text'=>$text, 'fromUser'=>$fromUser, 'setData'=>$setData, 'replyTxt'=>$replyTxt]);
