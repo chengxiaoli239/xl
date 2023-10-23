1ad5a42ef962355f59c7bbc1617e97810bc516d6
ak47 <wangyegao@126.com>
2023-10-23 15:16:24 +0800

    特殊字符

--- common/service/jobs/robots/user/EYunUserJobs.php
+++ common/service/jobs/robots/user/EYunUserJobs.php
@@ -48,8 +48,8 @@ class EYunUserJobs extends CommonJob {
                             'created_at' => $now_time,
                         ]);
                     }
-                    $setData['nickName'] = base64_encode($data['nickName']);
-                    $setData['remark'] = base64_encode($data['remark']);
+                    $setData['nickName'] = \common\tools\Common::filterEmoji($data['nickName']);
+                    $setData['remark'] = \common\tools\Common::filterEmoji($data['remark']);//base64_encode($data['remark']);
                     $setData['updated_at'] = $now_time;
                     $setData = array_merge($setData, $data);
                     Tool_Common::log('/eyun/'.self::class_basename(__CLASS__), 'INFO', '内容', ['setData'=>$setData]);
