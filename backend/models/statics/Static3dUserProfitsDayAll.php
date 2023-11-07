5cd3c679a9527daf8085440fb0349eb82ccf376c
ak47 <wangyegao@126.com>
2023-11-08 00:34:13 +0800

    优化

--- backend/models/statics/Static3dUserProfitsDayAll.php
+++ backend/models/statics/Static3dUserProfitsDayAll.php
@@ -14,6 +14,8 @@ use Yii;
  * @property string $wechat_user_name 微信id
  * @property string $bet_money 日投注金额
  * @property string $bonus 中奖金额
+ * @property string $up_money 上分
+ * @property string $down_money 下分
  * @property string $profits 利润
  * @property int $created_at 创建时间
  * @property int $updated_at 更新时间
@@ -36,11 +38,11 @@ class Static3dUserProfitsDayAll extends \common\models\base\BaseModel
     {
         return [
             [['date', 'update_time'], 'safe'],
-            [['user_id', 'wechat_user_id', 'lottery_type', 'created_at', 'updated_at'], 'integer'],
-            [['bet_money', 'bonus', 'profits'], 'number'],
+            [['user_id', 'wechat_user_id', 'created_at', 'updated_at'], 'integer'],
+            [['bet_money', 'bonus', 'up_money', 'down_money', 'profits'], 'number'],
             [['updated_at'], 'required'],
             [['wechat_user_name'], 'string', 'max' => 32],
-            [['date', 'user_id', 'lottery_type'], 'unique', 'targetAttribute' => ['date', 'user_id', 'lottery_type']],
+            [['date', 'user_id'], 'unique', 'targetAttribute' => ['date', 'user_id']],
         ];
     }
 
@@ -57,8 +59,9 @@ class Static3dUserProfitsDayAll extends \common\models\base\BaseModel
             'wechat_user_name' => Yii::t('app', '微信id'),
             'bet_money' => Yii::t('app', '日投注金额'),
             'bonus' => Yii::t('app', '中奖金额'),
+            'up_money' => Yii::t('app', '上分'),
+            'down_money' => Yii::t('app', '下分'),
             'profits' => Yii::t('app', '利润'),
-            'lottery_type' => Yii::t('app', '彩种类型26福彩27排三'),
             'created_at' => Yii::t('app', '创建时间'),
             'updated_at' => Yii::t('app', '更新时间'),
             'update_time' => Yii::t('app', '更新时间'),
