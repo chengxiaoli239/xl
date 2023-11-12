8012c853e5a483735f315fc66aa96de8c77f7e19
ak47 <wangyegao@126.com>
2023-11-12 23:54:47 +0800

    推送网盘

--- backend/service/agent/AgentUsersBalanceService.php
+++ backend/service/agent/AgentUsersBalanceService.php
@@ -143,7 +143,7 @@ class AgentUsersBalanceService extends BaseService {
                 throw_info('更新余额业务类型异常');
                 break;
         }
-        if(!WechatUser::updateAll(['balance'=>(new Expression("`balance`+".$changeMoney)), ['id'=>$member_id]])){
+        if(!WechatUser::updateAll(['balance'=>(new Expression("`balance`+".$changeMoney))], ['id'=>$member_id])){
             throw_info('余额更新错误');
         }
         $newWechatUser = WechatUser::findOne($member_id);
