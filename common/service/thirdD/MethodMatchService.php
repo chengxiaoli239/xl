ca98b7794fc89933147632f6d8cdd09754c2f44a
ak47 <wangyegao@126.com>
2023-11-17 19:09:55 +0800

    匹配优化

--- common/service/thirdD/MethodMatchService.php
+++ common/service/thirdD/MethodMatchService.php
@@ -588,7 +588,7 @@ class MethodMatchService extends CommonBaseService
     {
         list($originText, $singleCnTxt) = MethodMatchService::replaceSingleText($text); # 匹配号码前倍数字符先替换为空
         // 使用正则表达式匹配所有单个数字
-        if (preg_match_all('/胆(\d{1}(?:\s*\d{1})*)/', $text, $matches2)) {
+        if (preg_match_all('/胆\s*(\d{1}(?:\s*\d{1})*)/', $text, $matches2)) {
             $numbers = str_replace(' ', '', $matches2[1])[0];
         }
         #p([$text, $matchName]);
