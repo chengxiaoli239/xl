14bdfa7446ba28ba4209583a7cbfa7c00372cf59
ak47 <wangyego@126.com>
2023-12-19 21:35:54 +0800

    报表计算

--- common/service/wechat/eyun/EYunMessageOperateService.php
+++ common/service/wechat/eyun/EYunMessageOperateService.php
@@ -631,7 +631,7 @@ class EYunMessageOperateService  extends EYunBaseService
                 $allMoneys += $oneAllMoneys;
             }
             $transaction->commit();
-            push_queue_fast(UserDayStaticsJobs::class, ['user_id'=>$this->user_id, 'type'=>$vdata['type'], 'msg'=>'下单/撤单之后计算', 'wechat_user_id'=>$this->member_id]);
+            push_queue_fast(UserDayStaticsJobs::class, ['user_id'=>$this->user_id, 'type'=>$data['type'], 'msg'=>'下单/撤单之后计算', 'wechat_user_id'=>$this->member_id]);
         }catch (\Exception $e){
             $transaction->rollBack();
             Tool_Common::log('/eyun/'.__FUNCTION__, 'INFO', '消息处理-异常', ['user_id'=>$this->user_id, 'text'=>$text, 'fromUser'=>$fromUser, 'err_msg'=>$e->getMessage().$e->getFile().$e->getLine()]);
