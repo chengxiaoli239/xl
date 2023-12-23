e05cce2bbc24a353bc058687a0f862ae7d2e58a6
ak47 <wangyego@126.com>
2023-12-23 17:57:30 +0800

    程序调试

--- common/service/jobs/robots/message/WechatPrivateMsgReceiveJobs.php
+++ common/service/jobs/robots/message/WechatPrivateMsgReceiveJobs.php
@@ -43,7 +43,7 @@ class WechatPrivateMsgReceiveJobs extends CommonJob
             $data = $params['data']; # 消息内容体
             $fromUser = $data['fromUser'];
             $content = $data['content'];
-            $mkey = md5(self::class_basename(__CLASS__).'_'.$user_id.'_'.$fromUser.'_'.$content);
+            $mkey = md5(self::class_basename(__CLASS__).'_'.$user_id.'_'.$fromUser.'_'.json_encode($content));
             $num = \Yii::$app->redis->incr($mkey);
             if($num>1){
                 throw_info('短时间内重复操作，忽略处理', 50002);
