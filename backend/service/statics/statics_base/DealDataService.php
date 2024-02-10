54503ad31c8e9ec6ba1a156775eda1666fbab353
gxz2024 <data@126.com>
2024-02-10 11:39:12 +0800

    统计

--- backend/service/statics/statics_base/DealDataService.php
+++ backend/service/statics/statics_base/DealDataService.php
@@ -32,6 +32,7 @@ class DealDataService extends BaseService
         if(empty($qihao)){
             $qihao = HN0898Service::getCurrentQihao($lottery_type);
         }
+        Tool_Common::log('/data/'.__FUNCTION__, 'INFO', '逐期统计', ['lottery_type'=>$lottery_type, 'qihao'=>$qihao]);
         $DataDealStatus = DataDealStatus::findOne(['lottery_type'=>$lottery_type, 'qihao'=>$qihao]);
         $key = 'judgeDealTaskStatus_'.$lottery_type.'_'.$qihao;
         if(empty($DataDealStatus)){
