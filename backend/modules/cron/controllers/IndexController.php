bb23d77367d6c047d6ddd511ed15906e7506390f
gxz2024 <data@126.com>
2024-02-12 12:29:01 +0800

    优化

--- backend/modules/cron/controllers/IndexController.php
+++ backend/modules/cron/controllers/IndexController.php
@@ -208,10 +208,10 @@ class IndexController extends Controller
         $rst = ['status'=>200, 'msg'=>'操作成功'];
         $post = \Yii::$app->request->post();
 
-        for ($i=0; $i<7; $i++){
+        for ($i=0; $i<4; $i++){
             $rst['data'] = BetService::insertPlansTask($post['lottery_types']);
             //$rst['batch_simulate_data'] = BetService::batchSimulateBet($post['lottery_types']);
-            sleep(7);
+            sleep(12);
         }
 
         return $rst;
