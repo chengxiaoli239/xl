61f3d21ff79f2165decf68890d231422d848de57
gxz2024 <data@126.com>
2024-03-19 00:11:41 +0800

    升级

--- common/service/open/telegram/AoZhouKjService.php
+++ common/service/open/telegram/AoZhouKjService.php
@@ -16,11 +16,20 @@ class AoZhouKjService  extends TelegramBaseService
 
         $ds = ($kjData['codes_4nums_hz']%2==0) ? '双' : '单';
         $ft = ($kjData['codes_4nums_hz']%4)?:4;
-        $text = LotteryType::TYPE_OPTIONS[$this->lottery_type].'（前四位数番摊）'."\n\n".
+        $text = "============================\n";
+        $text .= LotteryType::TYPE_OPTIONS[$this->lottery_type].'（前四位数番摊）'."\n\n".
             "第 {$qiHao} 期\n".
             str_replace(',', '', $kjData['code_4n_str'])."总和{$kjData['codes_4nums_hz']}(".$ds.",".$ft.")\n\n".
-            "以下是历史课程表\n\n".
-            "1 2 2 3 4 5";
+            "以下是历史课程表\n\n";
+        $historyKjData = SscKjData::find()->where(['lottery_type'=>$this->lottery_type])->andWhere(['>', 'qihao', $qiHao-135])
+            ->asArray()->all();
+        foreach ($historyKjData as $k=>$historyKjDatum){
+            $kk = $k + 1;
+            $text .= self::getFanTan((int)$historyKjDatum['codes_4nums_hz']).' ';
+            if($kk%15==0){
+                $text .= "\n\n";
+            }
+        }
         Tool_Common::log('/kj_aozhou/'.__FUNCTION__, 'INFO', '开奖后群消息', ['lottery_type'=>$this->lottery_type, 'qihao'=>$qiHao, 'text'=>$text]);
 
         $config = \Yii::$app->params['TELEGRAM'];
@@ -32,5 +41,10 @@ class AoZhouKjService  extends TelegramBaseService
         ];
         #push_queue(SendMessageJobs::class, $params); # TG消息发送
 
+    }
+
+    public static function getFanTan($heZhi=0): int
+    {
+        return ($heZhi%4)?:4;
     }
 }
