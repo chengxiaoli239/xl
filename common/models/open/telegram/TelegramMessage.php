b8f1d8b7d530f8124c7c8d17130269eb17e45fc1
gxz2024 <data@126.com>
2024-03-23 09:46:39 +0800

    token

--- common/models/open/telegram/TelegramMessage.php
+++ common/models/open/telegram/TelegramMessage.php
@@ -9,13 +9,13 @@ use Yii;
  *
  * @property int $id
  * @property int $user_id user.id,本系统用户id
- * @property string $from_id 消息平台用户id
- * @property string $chat_id 消息来源id
+ * @property int $from_id 消息平台用户id
+ * @property int $chat_id 消息来源id
  * @property int $is_bot 是否机器人0、1
  * @property string $name 用户或群名称
  * @property string $type 消息类型:private、group
  * @property string $message_id 消息Id
- * @property string $update_id 更新Id
+ * @property int $update_id 更新Id
  * @property string $text 消息内容
  * @property string $content 全部内容
  * @property int $created_at 创建时间
@@ -38,13 +38,13 @@ class TelegramMessage extends \common\models\base\BaseModel
     public function rules()
     {
         return [
-            [['user_id', 'is_bot', 'created_at', 'updated_at'], 'integer'],
-            [['chat_id', 'text', 'created_at', 'updated_at'], 'required'],
+            [['user_id', 'from_id', 'chat_id', 'is_bot', 'update_id', 'created_at', 'updated_at'], 'integer'],
+            [['text', 'created_at', 'updated_at'], 'required'],
             [['text', 'content'], 'string'],
             [['update_at'], 'safe'],
-            [['from_id', 'chat_id', 'name'], 'string', 'max' => 32],
+            [['name'], 'string', 'max' => 32],
             [['type'], 'string', 'max' => 24],
-            [['message_id', 'update_id'], 'string', 'max' => 64],
+            [['message_id'], 'string', 'max' => 64],
             [['user_id', 'message_id'], 'unique', 'targetAttribute' => ['user_id', 'message_id']],
         ];
     }
