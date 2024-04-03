28b925b2adc43ae62c2b96839fc716837c026f54
gxz2024 <data@126.com>
2024-04-03 23:55:26 +0800

    数据

--- common/service/open/telegram/AoZhouKjService.php
+++ common/service/open/telegram/AoZhouKjService.php
@@ -32,7 +32,7 @@ class AoZhouKjService  extends BaseService
                 $text .= "\n\n";
             }
         }
-        $text = "\n\n============================";
+        $text .= "\n\n============================";
         Tool_Common::log('/kj_aozhou5/'.__FUNCTION__, 'INFO', '开奖后群消息', ['lottery_type'=>$this->lottery_type, 'qihao'=>$qiHao, 'text'=>$text]);
 
         $config = \Yii::$app->params['TELEGRAM'];
