614a7db7a9c9422ec07141ff38e62883f2e30193
gxz2024 <data@126.com>
2024-04-04 17:20:45 +0800

    开奖处理

--- common/service/lottery/aozhou5/AoZhou5Service.php
+++ common/service/lottery/aozhou5/AoZhou5Service.php
@@ -53,7 +53,7 @@ class AoZhou5Service extends CommonLotteryService
         try {
             $lotteryType = self::LOTTERY_TYPE_AOZHOU5;
             list($currentKjQiHao, $nextQiHao) = QihaoService::getKjQiHao($lotteryType);
-            if(!SscKjData::find()->where(['lottery_type'=>$lotteryType, 'qihao'=>$currentKjQiHao])->asArray()->one())){
+            if(!SscKjData::find()->where(['lottery_type'=>$lotteryType, 'qihao'=>$currentKjQiHao])->asArray()->one()){
                 return false;
             }
             $bets = Bets::find()->where(['status'=>0, 'lottery_type'=>$lotteryType]) # , 'qihao'=>$currentKjQiHao
