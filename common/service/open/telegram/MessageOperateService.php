ad80947980baf62afc838062eea7fb62363822eb
gxz2024 <data@126.com>
2024-04-13 13:59:52 +0800

    优化123

--- common/service/open/telegram/MessageOperateService.php
+++ common/service/open/telegram/MessageOperateService.php
@@ -159,7 +159,7 @@ class MessageOperateService  extends BaseService
 
         //$text = "1正/20 2念1/20";
         list($code, $vData, $msg) = $this->matchOtherOperate($text, ['message'=>$message]);
-        if($code == CommonBaseService::CODE_FOR_USER){
+        if(in_array($code, [CommonBaseService::CODE_FOR_USER, CommonBaseService::CODE_FOR_IGNORE])){
             return [$code, $vData, $msg];
         }
         //p($this->platformUser);
