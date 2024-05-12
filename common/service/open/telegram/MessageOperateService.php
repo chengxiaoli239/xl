25fbd62b306cab59495858915726340cc4ef2910
gxz2024 <data@126.com>
2024-05-12 12:07:14 +0800

    开关

--- common/service/open/telegram/MessageOperateService.php
+++ common/service/open/telegram/MessageOperateService.php
@@ -154,10 +154,10 @@ class MessageOperateService  extends BaseService
                 return [$code, $code, $msg];
             default:
                 $status = (new LotteryBet())->checkLotteryStatus($this->lottery_type);
-                Tool_Common::log('/bet_aozhou5/'.__FUNCTION__, 'INFO', '盘口状态检测', ['lottery_type'=>$this->lottery_type, 'status'=>$status, 'statusTxt'=>LotteryBet::STATUS_OPTIONS[$status]]);
 
                 $mKey = CacheKeyService::lotteryOpenSwitch($this->lottery_type);
                 $switch = commonRedis()->get($mKey);
+                Tool_Common::log('/bet_aozhou5/'.__FUNCTION__, 'INFO', '盘口状态检测', ['lottery_type'=>$this->lottery_type, 'status'=>$status, 'statusTxt'=>LotteryBet::STATUS_OPTIONS[$status], 'switch'=>$switch]);
                 if($status != LotteryBet::STATUS_START && !$switch){
                     throw_info('后台尚未开盘', CommonBaseService::CODE_FOR_USER);
                 }
