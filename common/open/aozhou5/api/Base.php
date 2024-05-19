6bb677e7c0a5e51dcda33fec79e4fcf0abee5eae
gxz2024 <data@126.com>
2024-05-19 18:11:27 +0800

    优化

--- common/open/aozhou5/api/Base.php
+++ common/open/aozhou5/api/Base.php
@@ -9,6 +9,7 @@ use common\open\OpenBase;
 use common\service\jobs\log\ErrorLogStaticsJobs;
 use common\tools\Tool_Common;
 use GuzzleHttp\Client;
+use GuzzleHttp\Psr7\Request;
 use GuzzleHttp\RequestOptions;
 use Yii;
 use yii\base\Component;
@@ -60,7 +61,8 @@ class Base extends OpenBase
      */
     public function get( string $path, array $params = [], array $headers = [], array $options = []): array
     {
-        return $this->request('GET', $path, $params, $headers, $options);
+        #return $this->request('GET', $path, $params, $headers, $options);
+        return $this->requestV2('GET', $path, $params, $headers, $options);
     }
 
     /**
@@ -73,7 +75,8 @@ class Base extends OpenBase
      */
     public function post( string $path, array $params = [], array $headers = [], array $options = [] ): array
     {
-        return $this->request('POST', $path, $params, $headers, $options);
+        #return $this->request('POST', $path, $params, $headers, $options);
+        return $this->requestv2('POST', $path, $params, $headers, $options);
     }
     
     /**
@@ -152,6 +155,79 @@ class Base extends OpenBase
             $errorMsg = $e->getMessage();
             //$statusCode = $response->getStatusCode();  // 获取成功响应的状态码
             Tool_Common::log('/aozhou5/request', 'ERR', '接口请求-异常', ['url'=>$url, 'req'=>$params, 'result'=>$result ?? [], 'code'=>$code, 'content'=>$content, 'msg'=>$errorMsg, 'statusCode'=>$statusCode]);
+            $status = OutSiteRequestLog::REQUEST_STATUS_FAIL;
+            push_queue(ErrorLogStaticsJobs::class, ['err_msg'=>$errorMsg, 'statusCode'=>$statusCode, 'result'=>$result ?? [], 'url'=>$url, 'req'=>$params]);
+            if (($e instanceof \common\exceptions\InfoException)) {
+                $result = $e->data;
+            }
+
+            throw_info($errorMsg, $code);
+        } finally {
+            $status = $status ?? OutSiteRequestLog::REQUEST_STATUS_FAIL;
+            $endtime = microtime(true) * 10000;
+            self::resetResult($result);
+            // 记录请求日志
+            $result = (is_array($result)) ? Json::encode($result) : '';
+            $logData = [
+                'send_time' => (int)($now / 10000),
+                'api_method' => $apiMethod,
+                'response_micro_time' => (int)(($endtime - $now) / 10),
+                'param' => Json::encode($params),
+                'response_data' => $result,
+                'headers' => Json::encode($headers),
+                'request_method' => $method,
+                'full_url' => $url,
+                'status' => $status,
+                'remark' => $errorMsg ?? '',
+            ];
+            Aozhou5RequestLog::find()->createCommand()->insert(Aozhou5RequestLog::tableName(), $logData)->execute();
+        }
+    }
+
+    /**
+     * 请求方法
+     * @param string $method  请求方法
+     * @param array $params  请求参数
+     * @param array $headers 请求头
+     * @param array $options 请求选项
+     * @return array|string|null
+     */
+    protected function requestV2(string $method, $apiMethod, array $params = [], array $headers = [], array $options = []): ?array
+    {
+        $now = microtime(true) * 10000;
+        $url = sprintf('%s%s', $this->apiUrl, $apiMethod);
+        // 请求参数
+        $this->setHeaders($headers);
+
+        try {
+            $client = new Client();
+            $params['timeout'] = 10;
+            $options = array_merge(['headers' => $headers], $params);
+            #p([$method, $url, $options]);
+            $request = new Request('POST', $url, $headers);
+            $response = $client->sendAsync($request, $options)->wait();
+            $content = $response->getBody()->getContents();
+            $statusCode = $response->getStatusCode();  // 获取成功响应的状态码
+            //p(['content'=>$content]);
+
+            if (!empty($content)) {
+                if(is_json($content)){
+                    $result = Json::decode($content);
+                }else{
+                    $result = ['content'=>$content];
+                }
+            }else{
+                throw_info('异常', 30000);
+            }
+
+            Tool_Common::log('/aozhou5/'.__FUNCTION__, 'INFO', '接口v2请求', ['url'=>$url, 'req'=>$params, 'statusCode'=>$statusCode, 'result'=>$result]);
+            $status = OutSiteRequestLog::REQUEST_STATUS_SUCCESS;
+            return $result;
+        } catch(\Exception $e) {
+            $code = $e->getCode();
+            $errorMsg = $e->getMessage();
+            //$statusCode = $response->getStatusCode();  // 获取成功响应的状态码
+            Tool_Common::log('/aozhou5/'.__FUNCTION__, 'ERR', '接口请求-异常', ['url'=>$url, 'req'=>$params, 'result'=>$result ?? [], 'code'=>$code, 'content'=>$content, 'msg'=>$errorMsg, 'statusCode'=>$statusCode]);
             $status = OutSiteRequestLog::REQUEST_STATUS_FAIL;
             push_queue(ErrorLogStaticsJobs::class, ['err_msg'=>$errorMsg, 'statusCode'=>$statusCode, 'result'=>$result ?? [], 'url'=>$url, 'req'=>$params]);
             if (($e instanceof \common\exceptions\InfoException)) {
