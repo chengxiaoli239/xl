17fdacf39a7c79129d57a3fc67dc87e55d2f382d
gxz2024 <data@126.com>
2024-06-01 15:17:19 +0800

    优化

--- backend/service/Lucky5/Lucky5Service.php
+++ backend/service/Lucky5/Lucky5Service.php
@@ -2159,7 +2159,7 @@ class Lucky5Service { # 重庆7时彩登陆体系
         $snInfo_snid = '';
         $rst = [];
         foreach ($codesArrs as $key=>$tmpcodesArr){
-            $bet_log = self::getBetLog($tz_type);
+            $bet_log = self::getBetLog($tz_type, $plan_id);
             if($playway == 4){ # 一字定
                 $url = self::getTzSiteInfo(self::$tz_system_id, 'ORDER_TZ');//.'?'.http_build_query($post_data);
                 $post_data = [
