f39169951781f82ef045923f17267f751c80aae6
gxz2024 <data@126.com>
2024-08-11 00:37:12 +0800

    bet

--- backend/service/clients/TzSystemUsersService.php
+++ backend/service/clients/TzSystemUsersService.php
@@ -560,7 +560,7 @@ class TzSystemUsersService extends ClientsBaseService{
             if($status != LotteryBet::STATUS_START && !$switch){
                 throw_info('后台尚未开盘', CommonBaseService::CODE_FOR_USER);
             }
-            //Tool_Common::log('/lotteryBet/'.__FUNCTION__, 'INFO', '获取下注任务', ['status'=>$status, 'switch'=>$switch]);
+            Tool_Common::log('/lotteryBet/'.__FUNCTION__, 'INFO', '获取下注任务', ['status'=>$status, 'switch'=>$switch]);
 
             $HI = date('H:i:s');
             if('09:00:00'<$HI && $HI<'09:01:00'){
