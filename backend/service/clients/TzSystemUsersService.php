536bc910f433402d3ab622fef790d77bf01f0a87
gxz2024 <data@126.com>
2024-08-22 21:08:46 +0800

    状态

--- backend/service/clients/TzSystemUsersService.php
+++ backend/service/clients/TzSystemUsersService.php
@@ -555,7 +555,6 @@ class TzSystemUsersService extends ClientsBaseService{
                 throw_info('没有任务yyy');
             }
             /*
-            */
             $status = (new LotteryBet())->checkLotteryStatus($lottery_type);
             $mKey = CacheKeyService::lotteryOpenSwitch($lottery_type);
             $switch = commonRedis()->get($mKey);
@@ -563,6 +562,7 @@ class TzSystemUsersService extends ClientsBaseService{
                 //throw_info('后台尚未开盘', CommonBaseService::CODE_FOR_USER);
             }
             Tool_Common::log('/lotteryBet/'.__FUNCTION__, 'INFO', '获取下注任务', ['username'=>$TzSystemsUsers->username, 'status'=>$status, 'switch'=>$switch]);
+            */
 
             $HI = date('H:i:s');
             if('09:00:00'<$HI && $HI<'09:01:00'){
