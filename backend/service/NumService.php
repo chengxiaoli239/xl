c08a69155a677ece08e78a6c4b9ccefb12e5afa8
gxz2024 <data@126.com>
2024-10-12 09:07:58 +0800

    类型添加

--- backend/service/NumService.php
+++ backend/service/NumService.php
@@ -2932,7 +2932,7 @@ class NumService extends BaseService {
 
             $type_desc .= '【区间止盈:'.$hz_Arr['area_profits'].'止损:'.$hz_Arr['area_loss'].'】';
 
-            $type_desc .= isset($hz_Arr['current_area_profits']) ? '，当前:'.number_format($hz_Arr['current_area_profits'], 2) : ''; # 当前遗漏
+            $type_desc .= isset($hz_Arr['current_area_profits']) ? '，当前区间盈利:'.number_format($hz_Arr['current_area_profits'], 2) : ''; # 当前遗漏
             $type_desc .= ($hz_Arr['betStatus']==1 OR $hz_Arr['areaBetStatus']==1) ? " 起投期:".($hz_Arr['start_qihao']??$hz_Arr['filters']['start_qihao']) : ''; # 当前遗漏
 
             $desc = $type_desc . ' '. $desc;
