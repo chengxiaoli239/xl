e0c5c09f08fad02f45e52f33afda52fe28ee7731
gxz2024 <data@126.com>
2024-10-13 10:20:46 +0800

    优化

--- backend/modules/forum/views/user-sys-plans/_search.php
+++ backend/modules/forum/views/user-sys-plans/_search.php
@@ -1,5 +1,6 @@
 <?php
 
+use backend\service\SscDataService;
 use yii\helpers\Html;
 use yii\widgets\ActiveForm;
 
@@ -22,11 +23,14 @@ $userNameList = \common\models\AdminModel::find()->select(['username'])->where('
             <?= $form->field($model, 'id')->label('计划ID') ?>
         </div>
         <div class="col-lg-2 col-xs-6">
-            <?= $form->field($model, 'account')->dropDownList($userNameList)->label('账号名称') ?>
+            <?= $form->field($model, 'account')->dropDownList($userNameList, ['prompt' => '-请选择-'])->label('账号名称') ?>
         </div>
         <?php }?>
         <div class="col-lg-2 col-xs-6">
-            <?= $form->field($model, 'status')->dropDownList([0=>'关闭', 1=>'开启'])->label('状态') ?>
+            <?= $form->field($model, 'status')->dropDownList([0=>'关闭', 1=>'开启'], ['prompt' => '-请选择-'])->label('状态') ?>
+        </div>
+        <div class="col-lg-2 col-xs-6">
+            <?= $form->field($model, 'plan_type')->dropDownList(SscDataService::PLAN_TYPE_OPTIONS, ['prompt' => '-请选择-'])->label('类型') ?>
         </div>
         <div class="col-lg-2 col-xs-6 d-flex align-items-center">
             <div class="form-group mb-0" style="margin-top: 20px">
