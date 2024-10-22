12448bd00aea085193972ea70ee6ba2e6f252534
gxz2024 <data@126.com>
2024-10-22 20:06:04 +0800

    优化

--- backend/service/statics/plan/OperatePlanService.php
+++ backend/service/statics/plan/OperatePlanService.php
@@ -91,8 +91,8 @@ class OperatePlanService extends BaseService
     public static function operatePlans_6(object $UserSysPlans, $current_kj_qihao): array
     {
         try {
-            if(!in_array($UserSysPlans->plan_type, [SscDataService::PLAN_TYPE_SINGLES_BET, SscDataService::PLAN_TYPE_BT_SINGLES_BET])){
-                throw_info('非中则投类型6、10,plan_type:'.$UserSysPlans->plan_type.'不处理');
+            if(!in_array($UserSysPlans->plan_type, [SscDataService::PLAN_TYPE_SINGLES_BET_WIN, SscDataService::PLAN_TYPE_BT_SINGLES_BET])){
+                throw_info('非中则投类型6、10,plan_type:'.$UserSysPlans->plan_type.'不处理:plan_id:'.$UserSysPlans->id);
             }
             $lottery_type = $UserSysPlans->lottery_type;
             $RedisLock = new RedisLock();
