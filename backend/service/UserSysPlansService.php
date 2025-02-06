5a95a805a344a2f98686db99194a71d40cd6614d
gxz2024 <data@126.com>
2025-02-06 23:07:59 +0800

    优化

--- backend/service/UserSysPlansService.php
+++ backend/service/UserSysPlansService.php
@@ -853,6 +853,9 @@ class UserSysPlansService extends BaseService {
         try {
             foreach ($codes as $key=>$code){
                 $code = trim($code);
+                if(empty($cooe)){
+                    continue;
+                }
                 if($change_per==1 && strpos($key, 'arise') !== false){
                     continue;
                 }
--- common/config/params.php
+++ common/config/params.php
@@ -19,7 +19,7 @@ return [
     'JUHE_KEY' => '4cf9ceff85b685abb8cb04abf9bb76cd',
 
     # 导入号码投注类型
-    'IMPORT_CODES_TYPES' => [ 19,27,34 ],
+    'IMPORT_CODES_TYPES' => [ 19,25,27,34 ],
     'IMPORT_CODES_TURN' => 10, # 轮换10组
 
     # 日志目录
