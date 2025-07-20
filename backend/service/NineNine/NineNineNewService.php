c9fc9c4fa515e2bfc95df7f7130d50bd0cc60e14
wangyegao <wangyegao@126.com>
2025-07-20 13:55:50 +0800

    优化

--- backend/service/NineNine/NineNineNewService.php
+++ backend/service/NineNine/NineNineNewService.php
@@ -368,6 +368,7 @@ class NineNineNewService extends BaseTZService {
                 $xCsrf_key = CommonService::buildXCsrfTokenKey($uid, $tz_system_id);
                 $m->set($xCsrf_key, $xCsrf, 120);
             }
+            $data = [];
             if($rstData['errorCode'] == 'FAIL' && $rstData['msg'] == 'Illegal X-Csrf-Token!!!'){
                 array_pop($headers); # 删除最后一个原始
                 array_pop($headers); # 删除最后一个原始
@@ -383,6 +384,10 @@ class NineNineNewService extends BaseTZService {
                     $xCsrf_key = CommonService::buildXCsrfTokenKey($uid, $tz_system_id);
                     $m->set($xCsrf_key, $xCsrf, 120);
                 }
+                $snRst = NineNineNewService::getSnidBySn($uid, $tz_system_id, $row->lottery_type);
+                if(isset($snRst['list'][0]['orderNo'])){
+                    $data['bet_rst']['snid'] = $snRst['list'][0]['orderNo']; // 获取方案内容
+                }
             }
             $tmpRst['bet_time'] = date('Y-m-d H:i:s');
             $status = ($tmpRst['rstData']['code'] == 200) ? 2 : 3;
@@ -396,6 +401,8 @@ class NineNineNewService extends BaseTZService {
         }catch (\Exception $e){
             return ['status'=>201, 'msg'=>$e->getMessage()];
         }
+
+        $rst['data'] = $data;
 
         return $rst;
     }
