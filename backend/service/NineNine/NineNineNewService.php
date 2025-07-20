1f159599b8411c82af8c6c4ca0fe103d36a1fe95
wangyegao <wangyegao@126.com>
2025-07-20 14:26:22 +0800

    优化

--- backend/service/NineNine/NineNineNewService.php
+++ backend/service/NineNine/NineNineNewService.php
@@ -1397,7 +1397,7 @@ class NineNineNewService extends BaseTZService {
         $TzSystemUser = TzSystemsUsers::findOne(['uid'=>self::$user_id, 'tz_system_id'=>$tz_system_id]);
 
         foreach ($orderNos as $key=>$orderNo) {
-            $orderNoArr = explode($orderNo, ';');
+            $orderNoArr = explode(';', $orderNo);
             $xCsrf = NineNineNewService::getXcsrfToken($uid, $tz_system_id);
             $url = $urlArr['baseUrl'] . '/cloud-lottery-service-server/gameInfo/userlottery/cancel/' . $orderNoArr[1];
             $headers = [
