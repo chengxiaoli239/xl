090585d72e53ed7e4f158cb07307d952727567ae
gaozi <wangyegao@126.com>
2025-10-22 15:49:37 +0800

    区间

--- backend/modules/forum/views/user-sys-plans/_form_2_19.php
+++ backend/modules/forum/views/user-sys-plans/_form_2_19.php
@@ -93,6 +93,10 @@ use yii\widgets\ActiveForm;
                         </div>
                     </div>
 
+                    <!--区间盈利止盈止损-->
+                    <?php if(isset(SscDataService::PLAN_TYPE_OPTIONS[SscDataService::PLAN_TYPE_AREA_SINGLES_BET])){ include(dirname(__FILE__).'/take_profits_area.php');} ?>
+
+
                     <?php include(dirname(__FILE__).'/take_or_stop_profits.php'); ?>
 
                     <?= $form->field($model, 'tz_sites')->checkboxList($tz_sites_Arr)->label('投注站点') ?>
